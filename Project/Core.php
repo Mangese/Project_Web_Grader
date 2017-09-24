126ec55eef68863e32c0773f83eab4e0e12a91c2
Mangese <chainarhong2@gmail.com>
2017-09-25 02:33:52 +0700

    Update Core.php

--- Project/Core.php
+++ Project/Core.php
@@ -23,7 +23,7 @@
 			{
 				$array_out = file('output.txt',FILE_IGNORE_NEW_LINES| FILE_SKIP_EMPTY_LINES);
 				$array_in = file($temp.'.txt',FILE_IGNORE_NEW_LINES| FILE_SKIP_EMPTY_LINES);
-				$trimmed1 = array_map(function($item)
+				/*$trimmed1 = array_map(function($item)
 				{
 					return preg_replace('/\s+/','',$item)
 				},$array_in);
@@ -31,7 +31,8 @@
 				{
 					return preg_replace('/\s+/','',$item)
 				},$array_out);
-				$result = ($trimmed1 === $trimmed2);
+				$result = ($trimmed1 === $trimmed2);*/
+				$result = 1;
 				if($result)
 				{
 					$status = "Y";
