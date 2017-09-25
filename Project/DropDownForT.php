6143095b8fa7d4c20894d1add99681866555b668
Mangese <chainarhong2@gmail.com>
2017-09-25 11:56:27 +0700

    Update DropDownForT.php

--- Project/DropDownForT.php
+++ Project/DropDownForT.php
@@ -5,7 +5,7 @@ $conn = mysql_connect("localhost","mangese","000000");
     session_start();
     $UID = $_SESSION["uid"];
     mysql_query("use grader;");
-    $result = mysql_query("select c.c_id as cid,c.ClassName as className from user u join section s on u.u_id = s.u_id join class c on s.c_id = c.c_id group by c.c_id where u.u_id = '$UID';");
+    $result = mysql_query("select c.c_id as cid,c.ClassName as className from user u join section s on u.u_id = s.u_id join class c on s.c_id = c.c_id where u.u_id = '$UID' group by c.c_id;");
     echo "var x = document.getElementById('selectClass');";
     while($row = mysql_fetch_assoc($result))
     {
