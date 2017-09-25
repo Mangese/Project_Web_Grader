fe4867723f5b7fe591900425abda4a5246ef89d2
Mangese <chainarhong2@gmail.com>
2017-09-25 12:03:19 +0700

    Update TeacherUploadedit.php

--- Project/TeacherUploadedit.php
+++ Project/TeacherUploadedit.php
@@ -104,7 +104,6 @@
       {
         $('#DataFromAjax tbody tr').remove();
         str = $("#selectClass").val();
-        alert(str);
         var xmlhttp = new XMLHttpRequest();
         xmlhttp.onreadystatechange = function(){
         if(this.readyState == 4 && this.status == 200){
