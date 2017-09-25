33b1824cdc841ff1905d69ee775a37882650e702
Mangored <chainarhong2@gmail.com>
2017-09-25 15:13:36 +0700

    Update

--- Project/createClass.php
+++ Project/createClass.php
@@ -5,11 +5,11 @@ $conn = mysql_connect("localhost","mangese","000000");
     session_start();
     $UID = $_SESSION["uid"];
     $TXT = $_REQUEST["text"];
-    $CID = $_REQUERT["class"];
+    $CID = $_REQUEST["class"];
     mysql_query("use grader;");
     $result = mysql_query("insert into section value('',(select concat(substring('ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789abcdefghijklmnopqrstuvwxyz', rand()*62+1, 1),substring('ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789abcdefghijklmnopqrstuvwxyz', rand()*62+1, 1), substring('ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789abcdefghijklmnopqrstuvwxyz', rand()*62+1, 1),        substring('ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789abcdefghijklmnopqrstuvwxyz', rand()*62+1, 1),    substring('ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789abcdefghijklmnopqrstuvwxyz', rand()*62+1, 1),    substring('ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789abcdefghijklmnopqrstuvwxyz', rand()*62+1, 1),  substring('ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789abcdefghijklmnopqrstuvwxyz', rand()*62+1, 1),  substring('ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789abcdefghijklmnopqrstuvwxyz', rand()*62+1, 1)   )),$CID,$UID,'$TXT');");
     $RS = mysql_error($result);
-    echo "<script> alert('$RS'); </script>"
+    echo "<script> alert('$RS'); </script>";
   }
     
     
