096aaecf4c6c8fa2986007c4251d44e0f566bc67
Mangese <chainarhong2@gmail.com>
2017-09-25 14:40:16 +0700

    Update TeacherUploadedit.php

--- Project/TeacherUploadedit.php
+++ Project/TeacherUploadedit.php
@@ -116,13 +116,11 @@
       }
        function fillDropHw()
       {
-        alert("Test");
         var xmlhttp = new XMLHttpRequest();
         xmlhttp.onreadystatechange = function()
         {
           if(this.readyState == 4 && this.status == 200)
           {
-            alert(this.responseText);
             eval(this.responseText);
           }
         }
