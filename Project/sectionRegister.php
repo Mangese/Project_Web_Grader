b962f2fb32bc7ef1ac396e360bf8a3bc5f7f10de
Mangese <chainarhong2@gmail.com>
2017-09-26 12:41:38 +0700

    Update sectionRegister.php

--- Project/sectionRegister.php
+++ Project/sectionRegister.php
@@ -7,26 +7,36 @@
     $PW = $_REQUEST["Section"];
     $UID = $_SESSION["uid"];
     mysql_query("use grader;");
-    $result = mysql_query("select count(*) as result,name as name,s_id as sid from section where password = '$PW' and '$UID' not in (select U_ID from register); ");
+    $result = mysql_query("select count(*) as result,name as name,s_id as sid from section where password = '$PW'; ");
     while($row = mysql_fetch_assoc($result))
     {
-      $temp = $row["result"];
-      echo "alert('$temp');";
-      if($row["result"] > 0)
-      {
-      $Text = $row["name"];
       $val = $row["sid"];
-      mysql_query("insert into section values('$val','$UID')");
-      echo "var x = document.getElementById('selectClass');";
-      echo "var option = document.createElement('option');";
-      echo "option.text = '$Text';";
-      echo "option.value = '$val';";
-      echo "x.add(option);";
-      }
-      else
+      $result1 = mysql_query("select count(*) as result from register where s_id <> '$val' and u_id <> '$UID'; ");
+      while($row1 = mysql_fetch_assoc($result1))
       {
-      echo "alert('Invalid Password');";
+       if($row1["result"] != 0)
+       {
+         if($row["result"] > 0)
+          {
+          $Text = $row["name"];
+          mysql_query("insert into section values('$val','$UID')");
+          echo "var x = document.getElementById('selectClass');";
+          echo "var option = document.createElement('option');";
+          echo "option.text = '$Text';";
+          echo "option.value = '$val';";
+          echo "x.add(option);";
+          }
+          else
+          {
+          echo "alert('Invalid Password');";
+          }
+       }
+        else
+        {
+         echo "alert('Already Registered');"; 
+        }
       }
+      
     }
   }
 ?>
