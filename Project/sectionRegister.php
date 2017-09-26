b5eacd59b16040256ce330281d8926998fec2212
Mangese <chainarhong2@gmail.com>
2017-09-26 12:55:24 +0700

    Update sectionRegister.php

--- Project/sectionRegister.php
+++ Project/sectionRegister.php
@@ -16,6 +16,8 @@
       {
         $val = $row["sid"];
         $result1 = mysql_query("select count(*) as result from register where s_id <> '$val' and u_id <> '$UID'; ");
+        $temp = $row1["result"];
+        echo "alert('$temp');";
         while($row1 = mysql_fetch_assoc($result1))
         {
           if($row1["result"] == 0)
