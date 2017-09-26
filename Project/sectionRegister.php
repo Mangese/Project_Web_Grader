fe80adea664a2b643b63b6bdffc763a5a800a7a5
Mangese <chainarhong2@gmail.com>
2017-09-26 13:01:49 +0700

    Update sectionRegister.php

--- Project/sectionRegister.php
+++ Project/sectionRegister.php
@@ -10,19 +10,14 @@
     $result = mysql_query("select count(*) as result,name as name,s_id as sid from section where password = '$PW'; ");
     while($row = mysql_fetch_assoc($result))
     {
-      $temp = $row["result"];
-      echo "alert('$temp');";
       if($row["result"] > 0)
       {
         $val = $row["sid"];
         $result1 = mysql_query("select count(*) as result from register where s_id = '$val' and u_id = '$UID'; ");
         while($row1 = mysql_fetch_assoc($result1))
         {
-          $temp = $row1["result"];
-        echo "alert('$temp');";
           if($row1["result"] == 0)
           {
-            echo "alert('test');";
             $Text = $row["name"];
             mysql_query("insert into register values($val,$UID);");
             echo "var x = document.getElementById('selectClass');";
@@ -30,7 +25,6 @@
             echo "option.text = '$Text';";
             echo "option.value = '$val';";
             echo "x.add(option);";
-            echo "alert('$Text $val');";
           }  
           else
           {
