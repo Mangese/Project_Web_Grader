26d829514162ed3c5265ab3356dabde0329b9508
Mangese <chainarhong2@gmail.com>
2017-09-26 12:33:12 +0700

    Update sectionRegister.php

--- Project/sectionRegister.php
+++ Project/sectionRegister.php
@@ -7,12 +7,14 @@
     $PW = $_REQUEST["Section"];
     $UID = $_SESSION["uid"];
     mysql_query("use grader;");
-    $result = mysql_query("select count(*) as result from section where password = '$PW' and '$UID' not in (select U_ID from register); ");
+    $result = mysql_query("select count(*) as result,name as name,s_id as sid from section where password = '$PW' and '$UID' not in (select U_ID from register); ");
     while($row = mysql_fetch_assoc($result))
     {
       if($row["result"] > 0)
       {
-      echo "var x = document.getElementById('selSectionHw');";
+      $Text = $row["name"];
+      $val = $row["sid"];
+      echo "var x = document.getElementById('selectClass');";
       echo "var option = document.createElement('option');";
       echo "option.text = '$Text';";
       echo "option.value = '$val';";
@@ -20,7 +22,7 @@
       }
       else
       {
-      echo "alert("Invalid Password");
+      echo "alert('Invalid Password');";
       }
     }
   }
