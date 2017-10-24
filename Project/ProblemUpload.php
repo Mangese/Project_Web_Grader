5515131c15f3b00185f8612d9fc91e0d787f9317
Mangese <chainarhong2@gmail.com>
2017-10-25 03:06:15 +0700

    Update ProblemUpload.php

--- Project/ProblemUpload.php
+++ Project/ProblemUpload.php
@@ -6,9 +6,35 @@
   	$CID = $_POST["ClassID"];
 	$PTYPE = $_POST["optradio"];
 	$PNAME = $_POST["ProblemNameUp"];
-$th_name =iconv("utf-8","TIS-620",$_POST['ProblemNameUp']);
-echo "<script> alert('$PNAME ลอง'); </script>";
-echo "<script> alert('$th_name ลอง'); </script>";
 
+$conn = mysql_connect("localhost","mangese","000000");
+	if($conn != FALSE)
+	{
+		mysql_query("use grader;");
+		mysql_query("SET NAMES TIS620");
+
+		$target = "Problem/";
+    $result = mysql_query("select concat((select classname from class where C_ID = '$CID'),'$UID',DATE_FORMAT(now(),'%Y%m%d'),count(*)) as genname from problem p where U_ID = '$UID' and C_ID = '$CID';");
+    while($row = mysql_fetch_assoc($result))
+    {
+      $GenFilename = $row['genname'];
+    }
+    $PDFN = $GenFilename."PDF.pdf";
+    $INN = $GenFilename."IN.in";
+    $OUTN = $GenFilename."OUT.out";
+		if(!move_uploaded_file($_FILES['PDFFile']['tmp_name'],$target.$PDFN))
+		{
+		echo "<script> alert('error1'); </script>";
+		}
+    if(!move_uploaded_file($_FILES['InFile']['tmp_name'],$target.$INN))
+		{
+		echo "<script> alert('error2'); </script>";
+		}
+    if(!move_uploaded_file($_FILES['OutFile']['tmp_name'],$target.$OUTN))
+		{
+		echo "<script> alert('error3'); </script>";
+		}
+	mysql_query("insert into problem(File_name,C_ID,U_ID,REMARK,inputfile,outputfile,uploaddate,language) values ('$PDFN',$CID,$UID,'$PNAME','$INN','$OUTN',DATE_FORMAT(now(),'%Y-%m-%d'),'$PTYPE');");
+	}
 	
 ?>
