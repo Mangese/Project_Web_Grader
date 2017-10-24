389368ee9b9619a61ef6b5f9e4899a719c6bb2c4
Mangese <chainarhong2@gmail.com>
2017-10-25 02:15:41 +0700

    Update ProblemUpload.php

--- Project/ProblemUpload.php
+++ Project/ProblemUpload.php
@@ -8,7 +8,7 @@
 	if($conn != FALSE)
 	{
 		mysql_query("use grader;");
-		$target = "File/";
+		$target = "Problem/";
     $result = mysql_query("select concat((select classname from class where C_ID = p.C_ID),U_ID,DATE_FORMAT(now(),'%Y%m%d'),count(*)) as genname from problem p where U_ID = '$UID' and C_ID = '$CID';");
     while($row = mysql_fetch_assoc($result))
     {
