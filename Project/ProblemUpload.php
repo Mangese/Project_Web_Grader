73dd51536b4984b768d03839528212ca0718d4f9
Mangese <chainarhong2@gmail.com>
2017-10-25 02:52:05 +0700

    Update ProblemUpload.php

--- Project/ProblemUpload.php
+++ Project/ProblemUpload.php
@@ -7,5 +7,7 @@
 	$PNAME = $_POST["ProblemNameUp"];
 $th_name =iconv("utf-8","TIS-620",$_POST['ProblemNameUp']);
 echo "<script> alert('$th_name'); </script>";
+echo "<script> alert('$PNAME'); </script>";
+
 	
 ?>
