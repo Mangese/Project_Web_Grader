8a00410407d915edb3c83543b7a35319ae33f9a1
Mangese <chainarhong2@gmail.com>
2017-10-25 02:44:31 +0700

    Update ProblemUpload.php

--- Project/ProblemUpload.php
+++ Project/ProblemUpload.php
@@ -6,6 +6,6 @@ header('charset=utf-8');
 	$PTYPE = $_POST["optradio"];
 	$PNAME = $_POST["ProblemNameUp"];
 $th_name =iconv("UTF-8","TIS-620",$_POST['ProblemNameUp']);
-echo "<script> alert('$th_name'); </script>";
+echo "<script> alert('$PNAME'); </script>";
 	
 ?>
