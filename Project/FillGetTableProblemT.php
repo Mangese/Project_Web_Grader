78f07b3a843e883cd1947fe04cad8371de569fc7
theepakornkong <theepakornkong@gmail.com>
2017-11-16 01:52:15 +0700

    FillGetTableProblemT.php call table

---
+++ Project/FillGetTableProblemT.php
@@ -0,0 +1,54 @@
+<?php
+  $conn = mysql_connect("localhost","mangese","000000");
+  if($conn != FALSE)
+  {
+    session_start();
+    mysql_query("use grader;");
+    mysql_query("set NAMES UTF8;");
+    $CID = $_REQUEST["class"];
+    $UID = $_SESSION["uid"];
+    $result = mysql_query("select p.Remark as problemName,p.language as lang ,p.u_id as u_teacher,s.s_id as section from problem p join user u on u.u_id = p.u_id join class c on c.c_id = p.c_id join section s on s.c_id = c.c_id where s.s_id = $CID and p.u_id = $UID;");
+    $RowNum = 0;
+    while($row = mysql_fetch_assoc($result))
+    {
+      $RowNum = $RowNum+1;
+      $PN = $row['problemName'];
+      // $UD = $row['uploadDate'];
+      $LN = $row['lang'];
+      // $FN = $row['fileName'];
+      echo "<tr>";
+      echo "<td style='width:30%'>";
+      echo "<a href = 'Problem/$FN' target = '_blank'>$PN</a>";
+      echo "</td>";
+      echo "<td style='width:10%'>";
+      echo "$LN";
+      echo "</td>";
+      echo "<td style='width:25%'>";
+      //echo "$LN";
+      echo "<div class='form-group'>";
+      echo "<div class='input-group date' id='datetimepicker1'>";
+      echo "<input type='text' class='form-control' placeholder='Date Send' />";
+      echo "<span class='input-group-addon'>";
+      echo "<span class='glyphicon glyphicon-calendar'></span>";
+      echo "</span>";
+      echo "</div>";
+      echo "</div>";
+      echo "</td>";
+      echo "<td style='width:25%'>";
+      // echo "<i class='fa fa-minus-circle' aria-hidden='true' style='color:#E74C3C'></i>";
+      echo "<div class='form-group'>";
+      echo "<div class='input-group date' id='datetimepicker3'>";
+      echo "<input type='text' class='form-control' placeholder='Time Send' />";
+      echo "<span class='input-group-addon'>";
+      echo "<span class='glyphicon glyphicon-time'></span>";
+      echo "</span>";
+      echo "</div>";
+      echo "</div>";
+      echo "</td>";
+      echo "<td style='width:25%'>";
+      echo "<input type='checkbox' name='vehicle' value=''><br>";
+      echo "</td>";
+      echo "</tr>";
+    }
+  }
+?>
