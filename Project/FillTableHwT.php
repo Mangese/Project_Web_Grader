14fd246ba5b26a7bdb06dc96461ee6b290c632ae
theepakornkong <theepakornkong@gmail.com>
2017-11-16 21:12:10 +0700

    Update FillTableHwT.php

--- Project/FillTableHwT.php
+++ Project/FillTableHwT.php
@@ -7,7 +7,8 @@
     mysql_query("set NAMES UTF8;");
     $CID = $_REQUEST["class"];
     $UID = $_SESSION["uid"];
-    $result = mysql_query("select p.Remark as problemName,p.UploadDate as uploadDate,p.language as lang,(case when (select count(*) from (select count(*),h_id,u_id from submit group by u_id,h_id) t where h_id = h.h_id group by h_id) is null then '0' else (select count(*) from (select count(*),h_id,u_id from submit group by u_id,h_id) t where h_id = h.h_id group by h_id) end) as count ,(case when (select count(*) from (select count(*),h_id,u_id,status from submit where status ='P' group by u_id,h_id) t where status = 'P' and h_id = h.h_id group by h_id) is null then '0' else (select count(*) from (select count(*),h_id,u_id,status from submit where status ='P' group by u_id,h_id) t where status = 'P' and h_id = h.h_id group by h_id) end)as count1 from homework h join problem p on p.p_id = h.p_id join user u on u.u_id = p.u_id join class c on c.c_id = p.c_id  where p.u_id = '$UID'  and h.s_id = '$CID' group by h.h_id;");  
+//     $result = mysql_query("select p.Remark as problemName,p.UploadDate as uploadDate,p.language as lang,(case when (select count(*) from (select count(*),h_id,u_id from submit group by u_id,h_id) t where h_id = h.h_id group by h_id) is null then '0' else (select count(*) from (select count(*),h_id,u_id from submit group by u_id,h_id) t where h_id = h.h_id group by h_id) end) as count ,(case when (select count(*) from (select count(*),h_id,u_id,status from submit where status ='P' group by u_id,h_id) t where status = 'P' and h_id = h.h_id group by h_id) is null then '0' else (select count(*) from (select count(*),h_id,u_id,status from submit where status ='P' group by u_id,h_id) t where status = 'P' and h_id = h.h_id group by h_id) end)as count1 from homework h join problem p on p.p_id = h.p_id join user u on u.u_id = p.u_id join class c on c.c_id = p.c_id  where p.u_id = '$UID'  and h.s_id = '$CID' group by h.h_id;");  
+    $result = mysql_query("select p.Remark as problemName,p.UploadDate as uploadDate,p.language as lang,h.assigndate,h.deadlinedate,(case when (select count(*) from (select count(*),h_id,u_id from submit group by u_id,h_id) t where h_id = h.h_id group by h_id) is null then '0' else (select count(*) from (select count(*),h_id,u_id from submit group by u_id,h_id) t where h_id = h.h_id group by h_id) end) as count ,(case when (select count(*) from (select count(*),h_id,u_id,status from submit where status ='P' group by u_id,h_id) t where status = 'P' and h_id = h.h_id group by h_id) is null then '0' else (select count(*) from (select count(*),h_id,u_id,status from submit where status ='P' group by u_id,h_id) t where status = 'P' and h_id = h.h_id group by h_id) end)as count1 from homework h join problem p on p.p_id = h.p_id join user u on u.u_id = p.u_id join class c on c.c_id = p.c_id  where p.u_id = '5'  and h.s_id = '111' group by h.h_id;");
     $RowNum = 0;
     while($row = mysql_fetch_assoc($result))
     {
@@ -18,6 +19,8 @@
       $SQTY = $row['count'];
       $LN = $row['lang'];
       $FN = $row['fileName'];
+      $AD = $row['assignedate'];
+      $DD = $row['deadlinedate'];
       echo "<tr style='width:100%'>";
          echo "<td style='width:800px'>";
            echo "$PN";
@@ -32,10 +35,12 @@
            echo "$PQTY";
          echo "</td>";
        echo "<td style='width:500px'>";
-           echo "Assign date";
+//            echo "Assign date";
+      echo "$AD";
          echo "</td>";
        echo "<td style='width:500px'>";
-           echo "Due date";
+//            echo "Due date";
+      echo "$DD";
          echo "</td>";
          echo "<td style='width:200px'>";
            echo "<button type='button' class='btn btn-danger'>Delete</button>";
