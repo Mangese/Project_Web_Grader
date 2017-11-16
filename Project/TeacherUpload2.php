addf67107f9dc53fee0fea901a0ad06df8001792
Mangese <chainarhong2@gmail.com>
2017-11-16 20:55:46 +0700

    Update TeacherUpload2.php

--- Project/TeacherUpload2.php
+++ Project/TeacherUpload2.php
@@ -1099,7 +1099,35 @@
 
     </div>
     <!--container-table-->
+	<div class="modal fade" id="modalChackDelete" role="dialog">
+	    <div class="modal-dialog modal-sm">
 
+	      <!-- Modal content-->
+	      <div class="modal-content">
+		<form name="from5" method="post" action="xxx.php">
+		  <!-- <div class="modal-header">
+		    <h4 class="modal-title">Delete Chacking</h4>
+		    <button type="button" class="close" data-dismiss="modal">&times;</button>
+		  </div> -->
+
+		  <div class="modal-body " style="text-align: center; margin-bottom:20px;">
+
+		    <h5 style="margin-bottom:20px">Do you want to delete?</h5>
+		    <button type="button" class="btn btn-success" data-dismiss="modal" style="margin-right:5px">Yes</button>
+		    <button type="button" class="btn btn-danger" data-dismiss="modal">No</button>
+
+
+		  </div>
+		  <!--End modal-body-->
+
+		  <!-- <div class="modal-footer">
+		    <!- -<button type="button" class="btn btn-success" data-dismiss="modal">OK</button>
+		    <button type="submit" class="btn btn-success" onclick="$('#modalChackDelete').modal('hide');">Delete</button>
+		  </div> -->
+		</form>
+	      </div>
+	    </div>
+	  </div>
   </body>
 
 </html>
