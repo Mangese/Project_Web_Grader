f4aabaecbbf01b61c0a0e176954d93f07eeacefa
Mangese <chainarhong2@gmail.com>
2017-11-16 22:55:27 +0700

    Update TeacherUpload2.php

--- Project/TeacherUpload2.php
+++ Project/TeacherUpload2.php
@@ -221,6 +221,7 @@
 
                     $('#TableHw tbody tr').remove();
                     str = $("#selSectionHw").val();
+	            alert(str);
                     var xmlhttp = new XMLHttpRequest();
                     xmlhttp.onreadystatechange = function () {
                       if (this.readyState == 4 && this.status == 200) {
