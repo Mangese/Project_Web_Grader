7b6e91701e73d9ef2b27b10d4b89a00239e8a9fb
theepakornkong <theepakornkong@gmail.com>
2017-11-17 18:59:25 +0700

    Update FillTable.php

--- Project/FillTable.php
+++ Project/FillTable.php
@@ -7,21 +7,25 @@
     mysql_query("set NAMES UTF8;");
     $Section = $_REQUEST["Section"];
     $UID = $_SESSION["uid"];
-    $result = mysql_query("select h.h_id as pid,p.Remark as problemName,p.File_Name as fileName,count(su.u_id) as num  ,(case when (select status from submit where su.H_ID = H_id and u_id = '$UID' and status = 'P' limit 1) is null then 'F' else 'P' end)  as status from homework h join section s on h.S_ID = s.S_ID join problem p on p.P_ID = h.P_ID left join submit su on su.H_ID = h.H_ID and su.u_id = '$UID'  where s.S_ID = '$Section'  group by h.h_id order by su.status desc;");
+    $result = mysql_query("select h.h_id as pid,p.Remark as problemName,p.File_Name as fileName,,concat(h.deadlinedate,' ',h.deadlinetime) as deadline,count(su.u_id) as num  ,(case when (select status from submit where su.H_ID = H_id and u_id = '$UID' and status = 'P' limit 1) is null then 'F' else 'P' end)  as status from homework h join section s on h.S_ID = s.S_ID join problem p on p.P_ID = h.P_ID left join submit su on su.H_ID = h.H_ID and su.u_id = '$UID'  where s.S_ID = '$Section'  group by h.h_id order by su.status desc;");
     $RowNum = 0;
     while($row = mysql_fetch_assoc($result))
     {
       $RowNum = $RowNum+1;
       echo "<tr>";
-      echo "<td style='width:15%'>";
+      echo "<td style='width:7%'>";
       echo "$RowNum";
       echo "</td>";
-      echo "<td style='width:40%' class = 'use'>";
+      echo "<td style='width:28%' class = 'use'>";
       $PN = $row['problemName'];
       $FN = $row['fileName'];
       $PID = $row['pid'];
       $QTY = $row['num'];
+      $DD = $row['deadline'];
       echo "<a href = 'Problem/$FN' target = '_blank'>$PN</a>";
+      echo "</td>";
+echo "<td style='width:35%'>";
+      echo "$DD";
       echo "</td>";
       echo "<td style='width:15%'>";
       echo "$QTY";
