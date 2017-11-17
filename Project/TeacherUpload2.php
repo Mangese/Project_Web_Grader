fec37ead146798daaf574dc7b575bda6947ed647
Mangese <chainarhong2@gmail.com>
2017-11-17 18:16:34 +0700

    Update TeacherUpload2.php

--- Project/TeacherUpload2.php
+++ Project/TeacherUpload2.php
@@ -239,7 +239,7 @@
                     var xmlhttp = new XMLHttpRequest();
                     xmlhttp.onreadystatechange = function () {
                       if (this.readyState == 4 && this.status == 200) {
-                        $('#Result').append(this.responseText);
+                        eval(this.responseText);
                       }
                     }
                     xmlhttp.open("POST", "FillTableResult.php?section=" + str, true);
