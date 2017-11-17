73d1bc41654488d3fb4e95be7650bdf54d76c5f8
Mangese <chainarhong2@gmail.com>
2017-11-17 18:33:08 +0700

    Update FillTableResult.php

--- Project/FillTableResult.php
+++ Project/FillTableResult.php
@@ -1,24 +1,2 @@
 <?php
-  $conn = mysql_connect("localhost","mangese","000000");
-  if($conn != FALSE)
-  {
-    session_start();
-    mysql_query("use grader;");
-    mysql_query("set NAMES UTF8;");
-    $SID = $_REQUEST["section"];
-    $UID = $_SESSION["uid"];
-    $sumProblem = mysql_query("select count(*) as sumPloblem from homework where s_id = '$SID';");
-    $sumStudentInSec = mysql_query("select u_id as sumStudentInSec from register where s_id = '$SID',");  
-    $RowNum = 0;
-
-    while($row = mysql_fetch_assoc($sumProblem)){
-      $sumPb = $row['sumPloblem'];
-      $_SESSION["sumpb"] = $sumPb;
-      echo "alert('$sumPb');";
-    }
-
-    while($row = mysql_fetch_assoc($sumStudentInSec)){
-      $sumStd = $row['sumStudentInSec'];
-    }
-   }
 ?>
