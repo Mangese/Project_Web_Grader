89e3bcdd0901c4b0042f64de1c050454a7b89bfa
Mangese <chainarhong2@gmail.com>
2017-11-17 18:23:44 +0700

    Update FillTableResult.php

--- Project/FillTableResult.php
+++ Project/FillTableResult.php
@@ -20,37 +20,5 @@
     while($row = mysql_fetch_assoc($sumStudentInSec)){
       $sumStd = $row['sumStudentInSec']
     }
-  //   while($row = mysql_fetch_assoc($result))
-  //   {
-  //     $RowNum = $RowNum+1;
-  //     $PN = $row['problemName'];
-  //     $UD = $row['uploadDate'];
-  //     $PQTY = $row['count1'];
-  //     $SQTY = $row['count'];
-  //     $LN = $row['lang']; 
-  //     $FN = $row['fileName'];
-  //     echo "<tr style='width:100%'>";
-  //        echo "<td style='width:800px'>";
-  //          echo "$PN";
-  //        echo "</td>";
-  //        echo "<td style='width:200px'>";
-  //          echo "$LN";
-  //        echo "</td>";
-  //        echo "<td style='width:700px'>";
-  //          echo "$SQTY";
-  //        echo "</td>";
-  //        echo "<td style='width:700px'>";
-  //          echo "$PQTY";
-  //        echo "</td>";
-  //      echo "<td style='width:500px'>";
-  //          echo "Assign date";
-  //        echo "</td>";
-  //      echo "<td style='width:500px'>";
-  //          echo "Due date";
-  //        echo "</td>";
-  //        echo "<td style='width:200px'>";
-  //          echo "<button type='button' class='btn btn-outline-secondary'>Detail</button>";
-  //      echo "</tr>";
-  //   }
    }
 ?>
