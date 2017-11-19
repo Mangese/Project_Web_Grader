93c3b6452a8410a34883da813690ce6909d55e8f
theepakornkong <theepakornkong@gmail.com>
2017-11-20 02:09:50 +0700

    Update FillLastSendFileResult.php

--- Project/FillLastSendFileResult.php
+++ Project/FillLastSendFileResult.php
@@ -5,11 +5,11 @@ if($conn != FALSE)
   session_start();
   mysql_query("use grader;");
   mysql_query("set NAMES UTF8;");
-  $CID = $_REQUEST["sectionreq"];
+//   $CID = $_REQUEST["sectionreq"];
   $UID = $_REQUEST["uidreq"];
   $HID = $_REQUEST["hidreq"];
   // $UID = $_SESSION["uid"];
-  $result = mysql_query("select su.u_id,su.h_id,p.remark,su.status,concat(su.submit_date,' ',su.submit_time) as submit,su.source_file from submit su join homework h on su.h_id = h.h_id join problem p on h.p_id = p.p_id join section s on h.s_id = s.s_id where su.u_id ='$UID' and s.s_id = '$CID' and su.h_id = '$HID' order by submit desc limit 3;");
+  $result = mysql_query("select su.u_id,su.h_id,p.remark,su.status,concat(su.submit_date,' ',su.submit_time) as submit,su.source_file from submit su join homework h on su.h_id = h.h_id join problem p on h.p_id = p.p_id join section s on h.s_id = s.s_id where su.u_id ='$UID' and su.h_id = '$HID' order by submit desc limit 3;");
   $RowNum = 0;
   while($row = mysql_fetch_assoc($result))
   {
