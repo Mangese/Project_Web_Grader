a281ea95474d9398bd7a47650068c2d66207761f
Mangese <chainarhong2@gmail.com>
2017-11-19 23:07:09 +0700

    Update logout.php

--- Project/logout.php
+++ Project/logout.php
@@ -2,6 +2,8 @@
 	session_start();
 	unset($_SESSION["user"]);
 	unset($_SESSION["uid"]);
+	unset($_SESSION["firstname"]);
+	unset($_SESSION["lastname"]);
 	unset($_SESSION["utype"]);
 	SESSION_destroy();
 	echo "<script> window.location = 'login.php'; </script>";
