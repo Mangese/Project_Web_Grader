c54504ceb308fccbe055a17bf45318e9aad8d8a3
Mangese <chainarhong2@gmail.com>
2017-11-19 17:42:36 +0700

    Update testUnzip.php

--- Project/testUnzip.php
+++ Project/testUnzip.php
@@ -2,5 +2,6 @@
   $baseTarget = "Problem/";
   $FileName = "EGCO1111201711192IN.zip";
   $UnzipTarget = "UnzipInputField/";
+  exec("rm $baseTarget$UnzipTarget *");
   exec("unzip $baseTarget$FileName -d $baseTarget$UnzipTarget");
 ?>
