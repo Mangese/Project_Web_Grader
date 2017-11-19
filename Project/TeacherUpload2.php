6ea2724701911f4e02ba3870773e895ca428c6eb
theepakornkong <theepakornkong@gmail.com>
2017-11-20 02:12:33 +0700

    Update TeacherUpload2.php

--- Project/TeacherUpload2.php
+++ Project/TeacherUpload2.php
@@ -112,7 +112,7 @@
                         $('#tb3LastSendFile').append(this.responseText);
                       }
                     }
-                    xmlhttp.open("POST", "FillLastSendFileResult.php?uidreq=" + x + "&hidreq=" + y + "&sectionreq=" + z, true);
+                    xmlhttp.open("POST", "fillLastSendFileResult.php?uidreq=" + x + "&hidreq=" + y + "&countrow=" + z, true);
                     xmlhttp.send();
                   }
 
