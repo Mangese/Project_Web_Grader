64e8a061aa0526ab5bcc16d583c903dbaf58ab81
Mangese <chainarhong2@gmail.com>
2017-11-21 03:12:48 +0700

    Update FillLastSendFileResult.php

--- Project/FillLastSendFileResult.php
+++ Project/FillLastSendFileResult.php
@@ -9,6 +9,7 @@ if($conn != FALSE)
   $HID = $_REQUEST["hidreq"];
   // $UID = $_SESSION["uid"];
   $result = mysql_query("select su.u_id,su.h_id,p.remark,su.status,su.submit_date,su.submit_time,su.source_file from submit su join homework h on su.h_id = h.h_id join problem p on h.p_id = p.p_id join section s on h.s_id = s.s_id where su.u_id ='$UID' and su.h_id = '$HID' order by submit_date,submit_time desc limit 3;");
+  $fileDownload = mysql_query("");
   $RowNum = 0;
   while($row = mysql_fetch_assoc($result))
   {
@@ -31,7 +32,7 @@ if($conn != FALSE)
     echo "$STA";
     echo "</td>";
     echo "<td style='width:20%'>";
-    echo "<a href='http://www.google.com'><i class='fa fa-download' aria-hidden='true'></i></a>";
+    echo "<a href='$FN' download><i class='fa fa-download' aria-hidden='true'></i></a>";
     echo "</td>";
    
     echo "</tr>";
