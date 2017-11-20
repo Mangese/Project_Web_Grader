c08cd64f9e5e6bb490aaf79abd2977a47fe2b719
theepakornkong <theepakornkong@gmail.com>
2017-11-20 17:09:27 +0700

    Update TeacherUpload2.php

--- Project/TeacherUpload2.php
+++ Project/TeacherUpload2.php
@@ -100,12 +100,12 @@
                       format: 'HH:mm'
                     });
                   }
-                  function ResultModalHeader(x, y, z, qq) {
+                  function ResultModalHeader(x, y, z, qq, name) {
                     // alert(x);
                     // alert(y);
-                     alert(z);
+                     alert(name);
 			  alert(qq);
-			  document.getElementById('modalValueResult').innerHTML = z+ " ex "+ qq;
+			  document.getElementById('modalValueResult').innerHTML = name+ " ex "+ qq;
                     $('#tb3LastSendFile tbody tr').remove();
                     str = $("#selSectionHw").val();
                     var xmlhttp = new XMLHttpRequest();
