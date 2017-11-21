6af85f2609bb929ec03352835d1fde9793e75250
Mangese <chainarhong2@gmail.com>
2017-11-21 19:21:33 +0700

    Update TeacherUpload2.php

--- Project/TeacherUpload2.php
+++ Project/TeacherUpload2.php
@@ -63,6 +63,9 @@
 
   <body>
     <!--Start script-->
+	  	  	<script src="https://code.jquery.com/jquery-1.12.4.js"></script>
+	  	<script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+
     <script>
                   $(document).ready(function () {
                     fillDropDownSection();
