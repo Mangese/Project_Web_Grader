c85e935ce2fc5414aafe7a4eb9bceed85ee8523b
theepakornkong <theepakornkong@gmail.com>
2018-01-14 19:58:19 +0700

    Update StudentUpload1.php

--- Project/StudentUpload1.php
+++ Project/StudentUpload1.php
@@ -209,6 +209,7 @@
                   <input type='hidden' name = "ProblemName" id = "ProblemName">
 		  <input type='hidden' name = "SectionValue" id = "SectionValue">
 		  <input type='file' name = "Uploaded_file" id = "Uploaded_file" accept=".c" required>
+		  <input type='text' name = "Main_file" id = "Main_file" placeholder="File name of main class">
                   <span class='file-custom'></span>
                   </label>
                 </div>
