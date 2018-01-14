f61b0398a31611b982ac8062f8db11fd31a58561
theepakornkong <theepakornkong@gmail.com>
2018-01-14 21:59:17 +0700

    Update StudentUpload1.php

--- Project/StudentUpload1.php
+++ Project/StudentUpload1.php
@@ -125,9 +125,9 @@
       document.getElementById('modalValue').innerHTML = $('#TableUploadHeader').val();
       $("#ProblemName").val(y);
       $("#SectionValue").val($("#selectClass").val());
-	    alert($("#TableUploadHeader").val());
-      alert($("#ProblemName").val());
-      alert($("#SectionValue").val());
+// 	    alert($("#TableUploadHeader").val()); ชื่อโจท์
+//       alert($("#ProblemName").val()); ไอดีโจทย์
+//       alert($("#SectionValue").val()); ไอดีเซค
     }
   </script>
 
