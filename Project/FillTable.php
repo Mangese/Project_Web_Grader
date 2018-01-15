2e15b29d7e426f2906666b9eaac0558fc1e3a436
theepakornkong <theepakornkong@gmail.com>
2018-01-15 22:04:47 +0700

    Update FillTable.php

--- Project/FillTable.php
+++ Project/FillTable.php
@@ -46,9 +46,7 @@ echo "<td style='width:22%'>";
       echo "<td style='width:13%'>";
       if(!strcmp($Status, "F"))
       {
-        if(strcmp($LA1, "Java")){
          echo "<button type='button' class='btn btn-outline-secondary'  onclick = 'alert(); ModalHeaderFunc(this,$PID,$LA1);' data-toggle='modal' ";
-        }
       }
       else
       {
