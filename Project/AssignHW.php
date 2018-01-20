d03cb6acea1377e8e1ea2373c07e2cffa48513ba
theepakornkong <theepakornkong@gmail.com>
2018-01-20 16:37:34 +0700

    Update AssignHW.php

--- Project/AssignHW.php
+++ Project/AssignHW.php
@@ -7,6 +7,7 @@ $conn = mysql_connect("localhost","mangese","000000");
     $SID = $_REQUEST["sid"];
     $DATE = $_REQUEST["date"];
     $TIME = $_REQUEST["time"];
+    $FULLMARK = $_REQUEST["fullMark"];
     mysql_query("use grader;");
     mysql_query("insert into homework (P_ID,LANGUAGE,S_ID,AssignDate,AssignTime,DeadlineDate,DeadlineTime) values ($PID,(select Language from problem where p_id = '1'),$SID,now(),now(),'$DATE','$TIME');");
   }
