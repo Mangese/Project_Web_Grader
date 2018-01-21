90b7c6a401114a7c5c0b886b29e909edd419d20e
theepakornkong <theepakornkong@gmail.com>
2018-01-21 12:46:16 +0700

    Update FillLastSendFileResult.php

--- Project/FillLastSendFileResult.php
+++ Project/FillLastSendFileResult.php
@@ -8,7 +8,8 @@ if($conn != FALSE)
   $UID = $_REQUEST["uidreq"];
   $HID = $_REQUEST["hidreq"];
   // $UID = $_SESSION["uid"];
-  $result = mysql_query("select su.u_id,su.h_id,p.remark,su.status,su.submit_date,su.submit_time,su.source_file from submit su join homework h on su.h_id = h.h_id join problem p on h.p_id = p.p_id join section s on h.s_id = s.s_id where su.u_id ='$UID' and su.h_id = '$HID' order by submit_date,submit_time desc limit 3;");
+//   $result = mysql_query("select su.u_id,su.h_id,p.remark,su.status,su.submit_date,su.submit_time,su.source_file from submit su join homework h on su.h_id = h.h_id join problem p on h.p_id = p.p_id join section s on h.s_id = s.s_id where su.u_id ='$UID' and su.h_id = '$HID' order by submit_date,submit_time desc limit 3;");
+  $result = mysql_query("select su.u_id,su.h_id,p.remark,su.status,su.submit_date,su.submit_time,su.source_file ,concat(su.CorrectTestCase,'/',su.FullTestCase) as testCase from submit su join homework h on su.h_id = h.h_id join problem p on h.p_id = p.p_id join section s on h.s_id = s.s_id where su.u_id ='$UID' and su.h_id = '$HID' order by submit_date,submit_time desc limit 3;");  
   $fileDownload = mysql_query("");
   $RowNum = 0;
   while($row = mysql_fetch_assoc($result))
@@ -18,6 +19,7 @@ if($conn != FALSE)
     $STA = $row['status'];
     $SD = $row['submit_date'];
     $ST = $row['submit_time'];
+    $TC = $row['testCase'];
     $RowNum =  $RowNum + 1 ;
     echo "<tr>";
     echo "<td style='width:7%'>";
@@ -33,7 +35,7 @@ if($conn != FALSE)
     echo "$STA";
     echo "</td>";
     echo "<td style='width:15%'>";
-    echo "Test case";
+    echo "$TC";
     echo "</td>";
     echo "<td style='width:18%'>";
     echo "<a href='$Target$FN' download><i class='fa fa-download' aria-hidden='true'></i></a>";
