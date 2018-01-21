38d73cdaf93185de4d305910a6cfb4f00dd1889c
theepakornkong <theepakornkong@gmail.com>
2018-01-21 13:23:39 +0700

    Adminpage
    Adminpage

---
+++ Project/Admin.php
@@ -0,0 +1,81 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="theme-color" content="#000000">
+
+  <link rel="manifest" href="%PUBLIC_URL%/manifest.json">
+  <link rel="shortcut icon" href="%PUBLIC_URL%/favicon.ico">
+
+
+  <!--bootstrap 4-->
+  <script src="https://code.jquery.com/jquery-3.1.1.slim.min.js" integrity="sha384-A7FZj7v+d/sdmMqp/nOQwliLvUsJfDHW+k9Omg/a/EheAdgtzNs3hpfag6Ed950n"
+    - crossorigin="anonymous"></script>
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ"
+    crossorigin="anonymous">
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js" integrity="sha384-DztdAPBWPRXSA/3eYEEUWrWCy7G5KFbe8fFjk5JAIxUYHKkDx6Qin1DkWx51bBrb"
+    crossorigin="anonymous"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js" integrity="sha384-vBWWzlZJ8ea9aCX4pEW3rVHjgjt7zpkNpZk+02D9phzyeVkE+jo0ieGizqPLForn"
+    crossorigin="anonymous"></script>
+
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datetimepicker/4.17.47/css/bootstrap-datetimepicker.min.css"
+  />
+
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.js"></script>
+
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.18.1/moment.js"></script>
+
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datetimepicker/4.17.47/js/bootstrap-datetimepicker.min.js"></script>
+
+
+  <!--Font Awesome-->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+  <link rel="stylesheet" href="TeacherUpload.css">
+
+  <nav class="navbar navbar-light bg-light" style="background-color: #0C3343; color:#ffffff">
+    <form class="form-inline">
+      <span class="h3" class="navbar-brand mb-0">Teacher</span>
+      <label class="ml-auto " id="SessionUser"></label>
+      <button class="btn btn-outline-secondary logout-btn ml-4 my-2 my-sm-0" onclick="logout()" type="button">Logout</button>
+    </form>
+  </nav>
+</head>
+<!-- <?php
+  session_start();
+
+  if(!isset($_SESSION["user"]))
+  {
+  echo "<script> alert('Please Login First'); window.location = 'logout.php'; </script>";
+  }
+  else
+  {
+  echo "<script> document.getElementById('SessionUser').innerText = '".$_SESSION["firstname"]." ".$_SESSION["lastname"]."'; </script>";
+  $UT = $_SESSION["utype"];
+  if(strcmp($UT,"T"))
+  {
+	  echo "<script> alert('Invalid Page'); window.location = 'StudentUpload1.php'; </script>";
+  }
+  }
+?> -->
+
+  <body>
+    <!--Start script-->
+    <p>Admin page</p>
+    
+    <script>
+      function logout() {
+        window.location = "logout.php";
+      }
+      
+    </script>
+    <!--End script-->
+
+
+
+    
+  </body>
+
+</html>
