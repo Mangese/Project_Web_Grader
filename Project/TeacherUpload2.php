de9c9956a85b55bcc2f3a2aa1ee4b080a84cc6a2
theepakornkong <theepakornkong@gmail.com>
2018-01-22 16:13:40 +0700

    Update TeacherUpload2.php

--- Project/TeacherUpload2.php
+++ Project/TeacherUpload2.php
@@ -1101,18 +1101,22 @@
                     </tbody>
 			   <div class="container">
 
-			      <div class="row">
-				<div class="col-xs-1 col-sm-1 col-md-6 col-lg-6">
-				  <h5 id='submitCount'></h5>
-				</div>
-				<div class="col-xs-1 col-sm-1 col-md-6 col-lg-6">
-				  <input type="text" style="border:none; text-align:right;" name="" placeholder="Mark">
-				  <h5>/</h5>
-				  <h5 id='fullMark'>Full mark</h5>
-				</div>
-			      </div>
+                      <div class="row">
+                        <div class="col-xs-1 col-sm-1 col-md-6 col-lg-6">
+                          <h5 id='submitCount'></h5>
+                        </div>
+                        <div class="col-xs-1 col-sm-1 col-md-3 col-lg-3">
+                          <input type="text" style="border:none; text-align:right;" name="" placeholder="Mark">
+                        </div>
+                        <div class="col-xs-1 col-sm-1 col-md-1 col-lg-1">
+                          <h5>/</h5>
+                        </div>
+                        <div class="col-xs-1 col-sm-1 col-md-2 col-lg-2">
+                          <h5 id='fullMark'>Full mark</h5>
+                        </div>
+                      </div>
 
-			    </div>
+                    </div>
                     <br>
                   </table>
                 </div>
