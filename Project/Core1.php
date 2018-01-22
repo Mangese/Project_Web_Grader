b2b54820f7904ae7dd5de18055502377c3bb0233
Mangese <chainarhong2@gmail.com>
2018-01-22 17:34:49 +0700

    Create Core1.php

---
+++ Project/Core1.php
@@ -0,0 +1,27 @@
+<?php
+session_start();
+$UID = $_SESSION['uid'];
+$PN = $_POST["ProblemName"];
+$page = 0;
+$conn = mysql_connect("localhost","mangese","000000");
+if($conn != FALSE)
+{
+	mysql_query("use grader;");
+	$QueryName = mysql_query("select concat($UID,$PN,count(*),'.zip') as name from submit where u_id = '$UID' and h_id = '$PN';");
+	while($row = mysql_fetch_assoc($QueryName))
+	{
+		$GenFilename = $row['name'];
+	}
+	$target = "File/";
+	$temp = $_FILES['Uploaded_file']['name'];
+	$SC = $_POST["SectionValue"];
+	$tempName = $GenFilename;
+	if(!move_uploaded_file($_FILES['Uploaded_file']['tmp_name'],$target.$tempName))
+	{
+		echo "<script> alert('error'); </script>";
+	}
+	$temp = $tempName;
+	$file_name = $tempName;
+	$temp = substr($temp,0,strpos($temp,"."));
+}
+?>
