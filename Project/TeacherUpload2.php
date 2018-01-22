a5f962cf7c43cc1e19f73a9deb10efb9098e197f
theepakornkong <theepakornkong@gmail.com>
2018-01-22 16:22:34 +0700

    Update TeacherUpload2.php

--- Project/TeacherUpload2.php
+++ Project/TeacherUpload2.php
@@ -1102,7 +1102,7 @@
 			   <div class="container">
 
                       <div class="row">
-                        <div class="col-xs-1 col-sm-1 col-md-6 col-lg-6">
+                        <div class="col-xs-1 col-sm-1 col-md-4 col-lg-4">
                           <h5 id='submitCount'></h5>
                         </div>
                         <div class="col-xs-1 col-sm-1 col-md-3 col-lg-3">
