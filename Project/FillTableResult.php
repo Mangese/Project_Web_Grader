455eee8ad623b00fe297e9a157215a6cb819af0e
theepakornkong <theepakornkong@gmail.com>
2018-01-28 16:43:41 +0700

    Update FillTableResult.php

--- Project/FillTableResult.php
+++ Project/FillTableResult.php
@@ -10,7 +10,8 @@
     $sumPb = mysql_query("select count(*) as sumPloblem from homework h join problem p on p.p_id = h.p_id where h.s_id = '$SID' and p.deleteflag is null and h.deleteflag is null;");
     $result1 = mysql_query("select concat(firstname,' ',lastname) as name,u.u_id as stdId from user u join register r on r.u_id = u.u_id where r.s_id = '$SID';");
     $result3 = mysql_query("select h_id as hid from homework h join problem p on p.p_id = h.p_id where h.s_id = '$SID' and h.deleteflag is null and p.deleteflag is null order by h.h_id;");
-    
+    $fullMark = mysql_query("select fullMark from homework h join problem p on p.p_id = h.p_id where h.s_id = '$SID' and p.deleteflag is null and h.deleteflag is null;");
+      
     while($row = mysql_fetch_assoc($sumPb)){
       $sumPlob = $row['sumPloblem'];
       echo "<thead class='thead'>";
@@ -26,6 +27,7 @@
           for ($i = 1; $i <= $sumPlob; $i++){
             echo "<th style='min-width:30px'>";
               echo "Ex$i";
+            echo "$fullMark";
             echo "</th>";
           }
           $sortLastCol = $sumPlob+2;
