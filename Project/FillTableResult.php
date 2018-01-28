621447c6ba1f1be6d851996806ac1f65448b2eac
theepakornkong <theepakornkong@gmail.com>
2018-01-28 16:59:34 +0700

    Update FillTableResult.php

--- Project/FillTableResult.php
+++ Project/FillTableResult.php
@@ -13,8 +13,9 @@
     $fullMark = mysql_query("select fullMark from homework h join problem p on p.p_id = h.p_id where h.s_id = '$SID' and p.deleteflag is null and h.deleteflag is null;");
       
     while($row = mysql_fetch_assoc($sumPb)){
+      while($row1 = mysql_fetch_assoc($fullMark)){
       $sumPlob = $row['sumPloblem'];
-      
+      $fullMarkT = $row1['fullMark'];
       echo "<thead class='thead'>";
         echo "<tr style='width:100%'>";
           echo "<th style='width:100px' onclick='sortTable(0)'>";
@@ -28,7 +29,7 @@
           for ($i = 1; $i <= $sumPlob; $i++){
             echo "<th style='min-width:30px'>";
               echo "Ex$i";
-            echo "$fullmark";
+            echo "$fullMarkT";
             echo "</th>";
           }
           $sortLastCol = $sumPlob+2;
@@ -38,6 +39,7 @@
           echo "</th>";
         echo "</tr>";
       echo "</thead>";
+      }
     }
     $CountRowForModal = 0;
     while($row = mysql_fetch_assoc($result1)){
