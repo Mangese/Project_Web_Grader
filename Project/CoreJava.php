a6a69a11a08525c007472626a2bafb13704ff1e6
Mangese <chainarhong2@gmail.com>
2018-01-29 21:31:39 +0700

    Update CoreJava.php

--- Project/CoreJava.php
+++ Project/CoreJava.php
@@ -58,7 +58,7 @@ if($conn != FALSE)
 		$OutputFromSubmit = "output.txt";	
 		while((file_exists("$baseTarget$UnzipTargetIn$countNameIn")&&(file_exists("$target"))))
 		{
-			exec("timeout 1 java -classpath $baseTarget$UnzipTarget Computer",$outR,$reR);
+			exec("timeout 1 java -classpath $baseTarget$UnzipTarget Computer < $baseTarget$UnzipTargetIn$countNameIn > $target$OutputFromSubmit",$outR,$reR);
 			echo "<script> alert('Out error Run = $outR') </script>";
 			echo "<script> alert('Out result Run = $reR') </script>";
 			$count = $count+1;
