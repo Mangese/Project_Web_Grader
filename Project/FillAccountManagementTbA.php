e3fbea22c6ec459e0457845164804c9eccb46091
theepakornkong <theepakornkong@gmail.com>
2018-01-30 05:45:27 +0700

    Update FillAccountManagementTbA.php

--- Project/FillAccountManagementTbA.php
+++ Project/FillAccountManagementTbA.php
@@ -5,63 +5,40 @@
     session_start();
     mysql_query("use grader;");
     mysql_query("set NAMES UTF8;");
-    $Section = $_REQUEST["Section"];
-    $UID = $_SESSION["uid"];
-    $result = mysql_query("select h.h_id as pid,p.Remark as problemName,p.Language as language,p.File_Name as fileName,concat(h.deadlinedate,' ',h.deadlinetime) as deadline,count(su.u_id) as num  ,(case when (select status from submit where su.H_ID = H_id and u_id = '$UID' and status = 'P' limit 1) is null then 'F' else 'P' end)  as status from homework h join section s on h.S_ID = s.S_ID join problem p on p.P_ID = h.P_ID left join submit su on su.H_ID = h.H_ID and su.u_id = '$UID'  where s.S_ID = '$Section' and h.deleteflag is null and p.deleteflag is null group by h.h_id order by h.h_id;");
-    $RowNum = 0;
+    $result = mysql_query("select username as userName,student_id as Student_ID,firstname as firstName,lastname as lastName,department as department,email as Email from user;");
     while($row = mysql_fetch_assoc($result))
     {
-      $RowNum = $RowNum+1;
-      echo "<tr>";
-      echo "<td style='width:6%'>";
-      echo "$RowNum";
-      echo "</td>";
-      echo "<td style='width:27%' class = 'use'>";
-      $PN = $row['problemName'];
-      $LA = $row['language'];
-      $FN = $row['fileName'];
-      $LA1 = '"'.$LA.'"';
-      $PID = $row['pid'];
-      $QTY = $row['num'];
-      $DD = $row['deadline'];
-      echo "<a href = 'Problem/$FN' target = '_blank'>$PN</a>";
-      echo "</td>";
-echo "<td style='width:22%'>";
-      echo "$DD";
-      echo "</td>";
-      echo "<td style='width:21%'>";
-      echo "$QTY";
-      echo "</td>";
-      echo "<td style='width:11%'>";
-      $Status = $row['status'];
-      if(!strcmp($Status, "F"))
-      {
-        echo "Fail";
-      }
-      else
-      {
-        echo "Pass";
-      }
-      echo "</td>";
-      echo "<td style='width:13%'>";
-      if(!strcmp($Status, "F"))
-      {
-      if(!strcmp($LA, "Java")){
-         echo "<button type='button' class='btn btn-outline-secondary'  onclick = 'ModalHeaderFunc(this,$PID,$LA1);' data-toggle='modal' ";
-         echo "data-target='#javaUpload'>Upload</button>";
-      }else
-      {
-        echo "<button type='button' class='btn btn-outline-secondary'  onclick = 'ModalHeaderFunc1(this,$PID,$LA1);' data-toggle='modal' ";
-        echo "data-target='#notjavaUpload'>Upload</button>";
-      }
-      }
-      else
-      {
-         echo "<button type='button' class='btn btn-outline-secondary'  ";
-        echo ">Upload</button>";
-      }
-     
       
+      $UN = $row['userName'];
+      $SID = $row['Student_ID'];
+      $FN = $row['firstName'];
+      $LN = $row['lastName'];
+      $DM = $row['department'];
+      $EM = $row['Email'];
+      echo "<tr>";
+      echo "<td style='width:15%'>";
+      echo "$UN";
+      echo "</td>";
+      echo "<td style='width:15%'>";
+      echo "$SID";
+      echo "</td>";
+      echo "<td style='width:15%'>";
+      echo "$FN";
+      echo "</td>";
+      echo "<td style='width:15%'>";
+      echo "$LN";
+      echo "</td>";
+      echo "<td style='width:15%'>";
+      echo "$DM";
+      echo "</td>";
+      echo "<td style='width:15%'>";
+      echo "$EM";
+      echo "</td>";
+      echo "<td style='width:5%'>";
+      echo "edit";
+      echo "</td>";
+      echo "<td style='width:5%'>";
+      echo "delete";
       echo "</td>";
       echo "</tr>";
     }
