7a7dd3d278c9004d771dbcfa4ca57d33d6ff094d
tammopinkid <tammo.pinkid@gmail.com>
2018-01-30 17:44:50 +0700

    edit

--- Project/AdminPage.php
+++ Project/AdminPage.php
@@ -340,32 +340,32 @@
                   <div class="modal-body">
                     <div class="form-group row">
                       <div class="col-sm-12">
-                        <input type="text" class="form-control" id="txtFirstname" name="txtFirstname" placeholder="Firstname" required oninvalid="this.setCustomValidity('Firstname is empty,\nInput only (A-Z,a-z)');"
+                        <input type="text" class="form-control" id="txtFirstname2" name="txtFirstname" placeholder="Firstname" required oninvalid="this.setCustomValidity('Firstname is empty,\nInput only (A-Z,a-z)');"
                           oninput="setCustomValidity('')" minlength=2 maxlength=50 pattern="[A-Za-z]{2,}" />
                       </div>
                     </div>
                     <div class="form-group row">
                       <div class="col-sm-12">
-                        <input type="text" class="form-control" name="txtLastname" placeholder="Lastname" required oninvalid="this.setCustomValidity('Lastname is empty,\nInput only (A-Z,a-z)');"
+                        <input type="text" class="form-control" name="txtLastname2" placeholder="Lastname" required oninvalid="this.setCustomValidity('Lastname is empty,\nInput only (A-Z,a-z)');"
                           oninput="setCustomValidity('')" minlength=3 maxlength=50 pattern="[A-Za-z]{3,}" />
                       </div>
                     </div>
                     <div class="form-group row">
                       <div class="col-sm-12">
-                        <input type="text" class="form-control" name="txtUsername" placeholder="Username" required oninvalid="this.setCustomValidity('Username is empty,\nInput only (A-Z,a-z,0-9)\nmin length: 6');"
+                        <input type="text" class="form-control" name="txtUsername2" placeholder="Username" required oninvalid="this.setCustomValidity('Username is empty,\nInput only (A-Z,a-z,0-9)\nmin length: 6');"
                           oninput="setCustomValidity('')" minlength=6 maxlength=20 pattern="[A-Za-z,0,1,2,3,4,5,6,7,8,9]{6,}"
                         />
                       </div>
                     </div>
                     <div class="form-group row">
                       <div class="col-sm-12">
-                        <input type="text" class="form-control" name="txtStudentID" placeholder="Student ID (EX. 5713XXX)" required oninvalid="this.setCustomValidity('Student ID is empty,,\nInput only (0-9)');"
+                        <input type="text" class="form-control" name="txtStudentID2" placeholder="Student ID (EX. 5713XXX)" required oninvalid="this.setCustomValidity('Student ID is empty,,\nInput only (0-9)');"
                           oninput="setCustomValidity('')" minlength=7 maxlength=7 pattern="[0,1,2,3,4,5,6,7,8,9]{7}" />
                       </div>
                     </div>
                     <div class="form-group row">
                       <div class="col-sm-12">
-                        <select class="form-control" id="sel1" name="sel1" required oninvalid="this.setCustomValidity('Please select some department');"
+                        <select class="form-control" id="sel2" name="sel2" required oninvalid="this.setCustomValidity('Please select some department');"
                           oninput="setCustomValidity('')">
                                                 <option value="">Department</option>
                                                 <option value="Biomedical Engineering">Biomedical Engineering</option>
@@ -380,20 +380,20 @@
                     </div>
                     <div class="form-group row">
                       <div class="col-sm-12">
-                        <input type="email" class="form-control" name="txtEmail" placeholder="E-mail" required oninvalid="this.setCustomValidity('Enter your email');"
+                        <input type="email" class="form-control" name="txtEmail2" placeholder="E-mail" required oninvalid="this.setCustomValidity('Enter your email');"
                           oninput="setCustomValidity('')" maxlength=30/>
                       </div>
                     </div>
                     <div class="form-group row">
                       <div class="col-sm-12">
-                        <input type="password" class="form-control" id="txtPassword" name="txtPassword" placeholder="New Password" minlength=6 maxlength=30
+                        <input type="password" class="form-control" id="txtNewPassword" name="txtNewPassword" placeholder="New Password" minlength=6 maxlength=30
                           required oninvalid="this.setCustomValidity('Enter your password,\nmin length: 6');" oninput="setCustomValidity('')"
                           onkeyup='check();' />
                       </div>
                     </div>
                     <div class="form-group row">
                       <div class="col-sm-12">
-                        <input type="password" class="form-control" id="txtPassword2" name="txtPassword2" placeholder="Confirm New Password" minlength=6
+                        <input type="password" class="form-control" id="txtNewPassword2" name="txtNewPassword2" placeholder="Confirm New Password" minlength=6
                           maxlength=30 required oninput="setCustomValidity('')" onkeyup='check();' />
                       </div>
                     </div>
