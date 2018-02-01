f4d527f1b736663f37127106e580d9e190b306ec
theepakornkong <theepakornkong@gmail.com>
2018-02-01 21:35:54 +0700

    Update AdminPage.php

--- Project/AdminPage.php
+++ Project/AdminPage.php
@@ -5,7 +5,7 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>Document</title>
+  <title>Admin-Grader</title>
 
   <!--bootstrap 4-->
   <script src="https://code.jquery.com/jquery-3.1.1.slim.min.js" integrity="sha384-A7FZj7v+d/sdmMqp/nOQwliLvUsJfDHW+k9Omg/a/EheAdgtzNs3hpfag6Ed950n"
