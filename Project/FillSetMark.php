ff92a8ad8bf755e9227a0acef6c6b7e3e975e44d
theepakornkong <theepakornkong@gmail.com>
2018-02-01 16:31:00 +0700

    setmarkphp

---
+++ Project/FillSetMark.php
@@ -0,0 +1,17 @@
+<?php
+$conn = mysql_connect("localhost","mangese","000000");
+  if($conn != FALSE)
+  {
+    session_start();
+    $UID = $_REQUEST["uidreq"];
+    $HID = $_REQUEST["hidreq"];
+    $TM = $_REQUEST["setMarkreq"];
+    $message = "test alert in php";
+    echo "<script type='text/javascript'>alert('$message');</script>";
+    
+    
+    
+    // mysql_query("use grader;");
+    // mysql_query("insert into homework (P_ID,LANGUAGE,S_ID,AssignDate,AssignTime,DeadlineDate,DeadlineTime,FullMark) values ($PID,(select Language from problem where p_id = '1'),$SID,now(),now(),'$DATE','$TIME','$FULLMARK');");
+  }
+?>
