b8d7c07902fe0f8a4ea496456dd917b787a52776
theepakornkong <theepakornkong@gmail.com>
2018-02-01 19:28:09 +0700

    Update TeacherUpload2.php

--- Project/TeacherUpload2.php
+++ Project/TeacherUpload2.php
@@ -162,8 +162,8 @@
 
                     var xmlhttp = new XMLHttpRequest();
                     xmlhttp.onreadystatechange = function () {
-                      if (this.readyState == 4 && this.status == 200) {
-                        eval(responseText);
+                      if (readyState == 4 && status == 200) {
+                        alert("ok");
                       }
                     }
                     xmlhttp.open("POST", "FillSetMark.php?uidreq=" + uidreq, true);
