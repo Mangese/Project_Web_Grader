bbbe5fcaac14e622938b6dc90c186c3b520c301b
theepakornkong <theepakornkong@gmail.com>
2018-02-01 20:08:44 +0700

    Update FillSetMark.php

--- Project/FillSetMark.php
+++ Project/FillSetMark.php
@@ -5,17 +5,16 @@ $conn = mysql_connect("localhost","mangese","000000");
     session_start();
     
     $UID = $_REQUEST["uidreq"];
-     $PID = $_REQUEST["pidreq"];
+     $HID = $_REQUEST["pidreq"];
      $TM = $_REQUEST["setMark"];
-     echo "alert('in php');";
-    echo "alert($UID);";
-    echo "alert($PID);";
-    echo "alert($TM);";
+    //  echo "alert('in php');";
+    // echo "alert($UID);";
+    // echo "alert($PID);";
+    // echo "alert($TM);";
+
+     mysql_query("use grader;");
+     mysql_query("update submit set TeacherMark = '$TM' where u_id = '$UID' and h_id = '$HID';");
     
-    
-    
-    
-    // mysql_query("use grader;");
-    // mysql_query("insert into homework (P_ID,LANGUAGE,S_ID,AssignDate,AssignTime,DeadlineDate,DeadlineTime,FullMark) values ($PID,(select Language from problem where p_id = '1'),$SID,now(),now(),'$DATE','$TIME','$FULLMARK');");
+
   }
 ?>
