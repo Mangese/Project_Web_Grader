11857d9c7b582d2b3df27ce3dd358ff7718e4b30
theepakornkong <theepakornkong@gmail.com>
2018-02-01 21:19:24 +0700

    Update FillTableResult.php

--- Project/FillTableResult.php
+++ Project/FillTableResult.php
@@ -8,7 +8,7 @@
     $SID = $_REQUEST["section"];
     $UID = $_SESSION["uid"];
     $sumPb = mysql_query("select count(*) as sumPloblem from homework h join problem p on p.p_id = h.p_id where h.s_id = '$SID' and p.deleteflag is null and h.deleteflag is null;");
-    $result1 = mysql_query("select concat(firstname,' ',lastname) as name,u.u_id as stdId from user u join register r on r.u_id = u.u_id where r.s_id = '$SID';");
+    $result1 = mysql_query("select concat(firstname,' ',lastname) as name,u.student_id as stdId from user u join register r on r.u_id = u.u_id where r.s_id = '$SID';");
     $result3 = mysql_query("select h_id as hid from homework h join problem p on p.p_id = h.p_id where h.s_id = '$SID' and h.deleteflag is null and p.deleteflag is null order by h.h_id;");
     $fullMark = mysql_query("select (case when fullMark is null then '' else fullMark end) as Mark from homework h join problem p on p.p_id = h.p_id where h.s_id = '$SID' and p.deleteflag is null and h.deleteflag is null;");
     
