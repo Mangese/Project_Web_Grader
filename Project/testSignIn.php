d2324b3d93c319caf173eb5559c69e20790eee65
Mangese <chainarhong2@gmail.com>
2018-02-01 16:10:12 +0700

    Update testSignIn.php

--- Project/testSignIn.php
+++ Project/testSignIn.php
@@ -38,7 +38,7 @@ if(mysql_num_rows($result)==1)
 			{
 			echo "<script> window.location = 'StudentUpload1.php' </script>";
 			}
-			else if(strcmp($row['utp'],"A"))
+			else if(!strcmp($row['utp'],"A"))
 			{
 			echo "<script> window.location = 'AdminPage.php' </script>";
 			}
