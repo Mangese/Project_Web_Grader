25282b2031dcf17cbd2af934eabc8b53b08c5082
Mangese <chainarhong2@gmail.com>
2018-02-01 19:44:20 +0700

    Update TeacherUpload2.php

--- Project/TeacherUpload2.php
+++ Project/TeacherUpload2.php
@@ -163,8 +163,8 @@
                     var xmlhttp = new XMLHttpRequest();
                     xmlhttp.onreadystatechange = function () {
                       if (this.readyState == 4 && this.status == 200) {
-
                         alert("ok");
+			eval(this.responseText);
                       }
                     }
                     xmlhttp.open("POST", "FillSetMark.php?uidreq=" + uidreq, true);
