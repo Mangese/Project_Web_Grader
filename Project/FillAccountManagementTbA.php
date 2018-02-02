c7bcbc4ea8c4e64426f8a88913afb4177b4d76aa
theepakornkong <theepakornkong@gmail.com>
2018-02-03 01:44:22 +0700

    Update FillAccountManagementTbA.php

--- Project/FillAccountManagementTbA.php
+++ Project/FillAccountManagementTbA.php
@@ -14,7 +14,7 @@
     // echo "<script type='text/javascript'>alert('$SIDSR');</script>";
     // echo "<script type='text/javascript'>alert('$NSR');</script>";
     // $result = mysql_query(" select username as userName,student_id as Student_ID,firstname as firstName,lastname as lastName,department as department,email as Email from user  ;");
-    if($TSR != 'S'){
+    if($SIDSR == ''){
       $result = mysql_query("select username as userName,student_id as Student_ID,firstname as firstName,lastname as lastName,department as department,email as Email from user where user_type like '%$TSR%' and firstname like '%$NSR%' ; ");
     }else{
       $result = mysql_query("select username as userName,student_id as Student_ID,firstname as firstName,lastname as lastName,department as department,email as Email from user where user_type like '%$TSR%' and student_id like '%$SIDSR%' and firstname like '%$NSR%' ; ");
