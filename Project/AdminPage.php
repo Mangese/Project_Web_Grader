90e7a9626b0645c3973240179addc7474d5576a7
theepakornkong <theepakornkong@gmail.com>
2018-02-03 00:12:30 +0700

    Update AdminPage.php

--- Project/AdminPage.php
+++ Project/AdminPage.php
@@ -118,14 +118,14 @@ if(!isset($_SESSION["user"]))
                   alert(classNameSearch);
 
 
-                  // var xmlhttp = new XMLHttpRequest();
-                  // xmlhttp.onreadystatechange = function () {
-                  //   if (this.readyState == 4 && this.status == 200) {
-                  //     $('#classManagementTb').append(this.responseText);
-                  //   }
-                  // }
-                  // xmlhttp.open("POST", "FillClassManagementTb.php?typeSearch=" + typeSearch + "&sidSearch=" + sidSearch + "&nameSearch=" + nameSearch, true);
-                  // xmlhttp.send();
+                  var xmlhttp = new XMLHttpRequest();
+                  xmlhttp.onreadystatechange = function () {
+                    if (this.readyState == 4 && this.status == 200) {
+                      $('#classManagementTb').append(this.responseText);
+                    }
+                  }
+                  xmlhttp.open("POST", "FillClassManagementTbA.php?CIDSearch=" + CIDSearch + "&classNameSearch=" + classNameSearch, true);
+                  xmlhttp.send();
                 }
     </script>
 
