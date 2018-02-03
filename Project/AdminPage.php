042f1bd66b3c1d582eb500173393ee249632707f
theepakornkong <theepakornkong@gmail.com>
2018-02-03 18:36:30 +0700

    Update AdminPage.php

--- Project/AdminPage.php
+++ Project/AdminPage.php
@@ -376,7 +376,7 @@ if(!isset($_SESSION["user"]))
                 <div class="modal-body">
                   <div class="form-group row">
                     <div class="col-sm-10">
-                      <input type="text" class="form-control" id="editFirstname" name="editFirstname" placeholder="Firstname" required oninvalid="this.setCustomValidity('Firstname is empty,\nInput only (A-Z,a-z)');"
+                      <input type="text" class="form-control" id="editFirstname" name="editFirstname" placeholder="Firstname" oninvalid="this.setCustomValidity('Firstname is empty,\nInput only (A-Z,a-z)');"
                         oninput="setCustomValidity('')" minlength=2 maxlength=50 pattern="[A-Za-z]{2,}" />
                     </div>
                     <div class="col-sm-2">
