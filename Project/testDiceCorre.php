5ea0132d21436b50528d56cce528bfaf39a9d030
Mangese <chainarhong2@gmail.com>
2018-02-04 17:16:41 +0700

    Update testDiceCorre.php

--- Project/testDiceCorre.php
+++ Project/testDiceCorre.php
@@ -1,7 +1,7 @@
 <?php
   $conn = mysql_connect("localhost","mangese","000000");
 	mysql_query("use grader;");
-  $HID = $_POST["hid"];
+  $HID = '4';
   $QueryName = mysql_query("select source_file as fileName from submit where h_id = '$HID';");
 	while($row = mysql_fetch_assoc($QueryName))
 	{
