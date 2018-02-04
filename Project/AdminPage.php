f2c57cd6abc2ba68bf1dcf86761bcd000fd76936
theepakornkong <theepakornkong@gmail.com>
2018-02-04 14:14:13 +0700

    Update AdminPage.php

--- Project/AdminPage.php
+++ Project/AdminPage.php
@@ -398,7 +398,7 @@ if(!isset($_SESSION["user"]))
                   </div>
                   <div class="form-group row">
                     <div class="col-sm-10">
-                      <input type="text" class="form-control" id="editLastname" name="editLastname" placeholder="Lastname" required oninvalid="this.setCustomValidity('Lastname is empty,\nInput only (A-Z,a-z)');"
+                      <input type="text" class="form-control" id="editLastname" name="editLastname" placeholder="Lastname" disabled required oninvalid="this.setCustomValidity('Lastname is empty,\nInput only (A-Z,a-z)');"
                         oninput="setCustomValidity('')" minlength=3 maxlength=50 pattern="[A-Za-z]{3,}" />
                     </div>
                     <div class="col-sm-2">
@@ -407,7 +407,7 @@ if(!isset($_SESSION["user"]))
                   </div>
                   <div class="form-group row">
                     <div class="col-sm-10">
-                      <input type="text" class="form-control" id="editUsername" name="editUsername" placeholder="Username" required oninvalid="this.setCustomValidity('Username is empty,\nInput only (A-Z,a-z,0-9)\nmin length: 6');"
+                      <input type="text" class="form-control" id="editUsername" name="editUsername" placeholder="Username" disabled required oninvalid="this.setCustomValidity('Username is empty,\nInput only (A-Z,a-z,0-9)\nmin length: 6');"
                         oninput="setCustomValidity('')" minlength=6 maxlength=20 pattern="[A-Za-z,0,1,2,3,4,5,6,7,8,9]{6,}"
                       />
                     </div>
@@ -417,8 +417,8 @@ if(!isset($_SESSION["user"]))
                   </div>
                   <div class="form-group row">
                     <div class="col-sm-10">
-                      <input type="text" class="form-control" id="editStudentID" name="editStudentID" placeholder="Student ID (EX. 5713XXX)" required
-                        oninvalid="this.setCustomValidity('Student ID is empty,,\nInput only (0-9)');" oninput="setCustomValidity('')"
+                      <input type="text" class="form-control" id="editStudentID" name="editStudentID" disabled placeholder="Student ID (EX. 5713XXX)"
+                        required oninvalid="this.setCustomValidity('Student ID is empty,,\nInput only (0-9)');" oninput="setCustomValidity('')"
                         minlength=7 maxlength=7 pattern="[0,1,2,3,4,5,6,7,8,9]{7}" />
                     </div>
                     <div class="col-sm-2">
@@ -427,7 +427,7 @@ if(!isset($_SESSION["user"]))
                   </div>
                   <div class="form-group row">
                     <div class="col-sm-10">
-                      <select class="form-control" id="editDepartment" name="editDepartment" required oninvalid="this.setCustomValidity('Please select some department');"
+                      <select class="form-control" id="editDepartment" name="editDepartment" disabled required oninvalid="this.setCustomValidity('Please select some department');"
                         oninput="setCustomValidity('')">
                         <option value="">Department</option>
                         <option value="Biomedical Engineering">Biomedical Engineering</option>
@@ -445,7 +445,7 @@ if(!isset($_SESSION["user"]))
                   </div>
                   <div class="form-group row">
                     <div class="col-sm-10">
-                      <input type="email" class="form-control" name="editEmail" id="editEmail" placeholder="E-mail" required oninvalid="this.setCustomValidity('Enter your email');"
+                      <input type="email" class="form-control" name="editEmail" id="editEmail" placeholder="E-mail" disabled required oninvalid="this.setCustomValidity('Enter your email');"
                         oninput="setCustomValidity('')" maxlength=30/>
                     </div>
                     <div class="col-sm-2">
@@ -454,7 +454,7 @@ if(!isset($_SESSION["user"]))
                   </div>
                   <div class="form-group row">
                     <div class="col-sm-10">
-                      <input type="password" class="form-control" id="editPassword" name="editPassword" placeholder="New Password" minlength=6
+                      <input type="password" class="form-control" id="editPassword" name="editPassword" disabled placeholder="New Password" minlength=6
                         maxlength=30 required oninvalid="this.setCustomValidity('Enter your password,\nmin length: 6');" oninput="setCustomValidity('')"
                         onkeyup='check();' />
                     </div>
@@ -464,8 +464,8 @@ if(!isset($_SESSION["user"]))
                   </div>
                   <div class="form-group row">
                     <div class="col-sm-10">
-                      <input type="password" class="form-control" id="editPassword2" name="editPassword2" placeholder="Confirm New Password" minlength=6
-                        maxlength=30 required oninput="setCustomValidity('')" onkeyup='check();' />
+                      <input type="password" class="form-control" id="editPassword2" name="editPassword2" disabled placeholder="Confirm New Password"
+                        minlength=6 maxlength=30 required oninput="setCustomValidity('')" onkeyup='check();' />
                     </div>
                   </div>
                 </div>
