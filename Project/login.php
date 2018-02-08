096647bd166e5de3437b7ee04143951a255fd2bf
theepakornkong <theepakornkong@gmail.com>
2018-02-08 23:34:46 +0700

    Update login.php

--- Project/login.php
+++ Project/login.php
@@ -146,13 +146,13 @@
                     <select class="form-control" id="sel1" name="sel1" required oninvalid="this.setCustomValidity('Please select some department');"
                       oninput="setCustomValidity('')">
 		        <option value="">Department</option>
-		        <option value="Biomedical Engineering">Biomedical Engineering</option>
-		        <option value="Civil Engineering">Civil Engineering</option>
-		        <option value="Chemical Engineering">Chemical Engineering</option>
-		        <option value="Computer Engineering">Computer Engineering</option>
-		        <option value="Electrical Engineering">Electrical Engineering</option>
-		        <option value="Industrial Engineering">Industrial Engineering</option>
-		        <option value="Mechanical Engineering">Mechanical Engineering</option>
+		        <option value="BE">Biomedical Engineering</option>
+		        <option value="CE">Civil Engineering</option>
+		        <option value="CHE">Chemical Engineering</option>
+		        <option value="CO">Computer Engineering</option>
+		        <option value="EE">Electrical Engineering</option>
+		        <option value="IE">Industrial Engineering</option>
+		        <option value="ME">Mechanical Engineering</option>
 		      </select>
                   </div>
                 </div>
