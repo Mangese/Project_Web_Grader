0784852f83b0a66abadf56067d970f4572816117
theepakornkong <theepakornkong@gmail.com>
2018-02-13 03:29:05 +0700

    Update AdminPage.php

--- Project/AdminPage.php
+++ Project/AdminPage.php
@@ -325,15 +325,15 @@ if(!isset($_SESSION["user"]))
       var sidreq = document.getElementById('sidmoc').value;
       alert("sid : " + sidreq);
 
-      // var xmlhttp = new XMLHttpRequest();
-      // xmlhttp.onreadystatechange = function () {
-      //   if (this.readyState == 4 && this.status == 200) {
-      //     alert("success");
-      //     eval(this.responseText);
-      //   }
-      // }
-      // xmlhttp.open("POST", "EditClassManagementA.php?cidreq=" + cidreq, true);
-      // xmlhttp.send();
+      var xmlhttp = new XMLHttpRequest();
+      xmlhttp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+          alert("success");
+          eval(this.responseText);
+        }
+      }
+      xmlhttp.open("POST", "EditSectionManagementA.php?sidreq=" + sidreq, true);
+      xmlhttp.send();
 
     }
 
