bbcdd73a8f03f0a50d3f675a3d62730062c56070
Mangese <chainarhong2@gmail.com>
2018-02-12 22:57:41 +0700

    Update AdminPage.php

--- Project/AdminPage.php
+++ Project/AdminPage.php
@@ -236,7 +236,7 @@ if(!isset($_SESSION["user"]))
         var xmlhttp = new XMLHttpRequest();
         xmlhttp.onreadystatechange = function () {
           if (this.readyState == 4 && this.status == 200) {
-            eval(this.responseText);
+            //eval(this.responseText);
           }
         }
         xmlhttp.open("POST", "EditAccountManagementA.php?uidreq=" + uidreq, true);
