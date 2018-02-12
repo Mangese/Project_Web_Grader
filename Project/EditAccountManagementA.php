838f1d5e7cdd6588a4e8f478c0dbc65b58c6eaac
theepakornkong <theepakornkong@gmail.com>
2018-02-13 02:05:31 +0700

    Update EditAccountManagementA.php

--- Project/EditAccountManagementA.php
+++ Project/EditAccountManagementA.php
@@ -7,12 +7,32 @@ $conn = mysql_connect("localhost","mangese","000000");
     $UID = $_REQUEST["uidreq"];
     $FN = $_REQUEST["fnamereq"];
     $LN = $_REQUEST["lnamereq"];
+    $UN = $_REQUEST["unamereq"];
+
+    $SID = $_REQUEST["sidreq"];
+    $DP = $_REQUEST["departreq"];
+    $EM = $_REQUEST["emailreq"];
+    $PW = $_REQUEST["passreq"];
+
     $UID = "'".$UID."'";
     $FN = "'".$FN."'";
     $LN = "'".$LN."'";
+    $UN = "'".$UN."'";
+    
+    $SID = "'".$SID."'";
+    $DP = "'".$DP."'";
+    $EM = "'".$EM."'";
+    $PW = "'".$PW."'";
+
     echo "alert('uid '+ $UID);";
     echo "alert('fname '+ $FN);";
     echo "alert('lname '+ $LN);";
+    echo "alert('lname '+ $UN);";
+
+    echo "alert('sid '+ $SID);";
+    echo "alert('dp '+ $DP);";
+    echo "alert('email '+ $EM);";
+    echo "alert('PW '+ $PW);";
     
     
     // mysql_query("use grader;");
