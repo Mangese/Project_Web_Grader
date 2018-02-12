ba3e8cc619a920995cf8fc8aeeaa8f37d81230ea
theepakornkong <theepakornkong@gmail.com>
2018-02-13 04:02:34 +0700

    Update EditClassManagementA.php

--- Project/EditClassManagementA.php
+++ Project/EditClassManagementA.php
@@ -14,7 +14,7 @@ $conn = mysql_connect("localhost","mangese","000000");
     
     
     
-    // mysql_query("use grader;");
-    // mysql_query("insert into homework (P_ID,LANGUAGE,S_ID,AssignDate,AssignTime,DeadlineDate,DeadlineTime,FullMark) values ($PID,(select Language from problem where p_id = '1'),$SID,now(),now(),'$DATE','$TIME','$FULLMARK');");
+     mysql_query("use grader;");
+     mysql_query("update class set ContactName=$CN where c_id =$CID;");
   }
 ?>
