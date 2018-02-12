c2139a8f27cbcdd361744aeeba58e61afd35336f
theepakornkong <theepakornkong@gmail.com>
2018-02-13 04:34:05 +0700

    addclass

---
+++ Project/AddClassManagementA.php
@@ -0,0 +1,19 @@
+<?php
+$conn = mysql_connect("localhost","mangese","000000");
+  if($conn != FALSE)
+  {
+    session_start();
+    
+    $NC = $_REQUEST["addNewClass"];
+    
+    $NC = "'".$NC."'";
+    
+    echo "alert('NC '+ $NC);";
+    
+    
+    
+    //  mysql_query("use grader;");
+    //  mysql_query("iNSERT INTO class (classname) VALUES ($NC);");
+     
+  }
+?>
