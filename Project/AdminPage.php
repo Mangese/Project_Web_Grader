cda1393b0c7f768cc01bb3b80b2e011dfdee85f8
theepakornkong <theepakornkong@gmail.com>
2018-02-13 04:40:31 +0700

    Update AdminPage.php

--- Project/AdminPage.php
+++ Project/AdminPage.php
@@ -139,7 +139,19 @@ if(!isset($_SESSION["user"]))
 
     function addClass() {
       var addNewClass = document.getElementById('addClassName').value;
+      alert("in add class");
       alert(addNewClass);
+
+      var xmlhttp = new XMLHttpRequest();
+      xmlhttp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+          alert("success");
+          eval(this.responseText);
+        }
+      }
+      xmlhttp.open("POST", "AddClassManagementA.php?addNewClass=" + addNewClass, true);
+      xmlhttp.send();
+      location.reload();
     }
 
     function fillaccountManagementTb() {
