d33f17b1769108b0c6efad119c5dc46d5f6b78bc
theepakornkong <theepakornkong@gmail.com>
2018-02-12 18:25:37 +0700

    Add files via upload

---
+++ Project/EditAccountManagementA.php
@@ -0,0 +1,17 @@
+<?php
+$conn = mysql_connect("localhost","mangese","000000");
+  if($conn != FALSE)
+  {
+    session_start();
+    
+    $UID = $_REQUEST["uid"];
+    
+
+    $message = "test alert in php";
+    echo "alert($message);";
+    
+    
+    // mysql_query("use grader;");
+    // mysql_query("insert into homework (P_ID,LANGUAGE,S_ID,AssignDate,AssignTime,DeadlineDate,DeadlineTime,FullMark) values ($PID,(select Language from problem where p_id = '1'),$SID,now(),now(),'$DATE','$TIME','$FULLMARK');");
+  }
+?>
