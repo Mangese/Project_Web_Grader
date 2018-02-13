81678fcfd4c7040cea28d6e81ae184895e7c68cd
theepakornkong <theepakornkong@gmail.com>
2018-02-13 18:48:07 +0700

    Update EditAccountManagementA.php

--- Project/EditAccountManagementA.php
+++ Project/EditAccountManagementA.php
@@ -45,7 +45,7 @@ $conn = mysql_connect("localhost","mangese","000000");
     //  'uPdate user '' set '
     // echo "alert($subquery);";
     if ($FN!=''){
-        $subquery=$subquery. " Firstname = '$FN' ";
+        $subquery=$subquery. " Firstname =  ";
       echo "alert('eiei');";
     }
     $subquery=$subquery."'";
