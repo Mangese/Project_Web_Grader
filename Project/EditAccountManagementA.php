d5ba01be27d014b9c5849adf0aa8c825148bf34a
theepakornkong <theepakornkong@gmail.com>
2018-02-13 19:13:23 +0700

    Update EditAccountManagementA.php

--- Project/EditAccountManagementA.php
+++ Project/EditAccountManagementA.php
@@ -53,6 +53,7 @@ $conn = mysql_connect("localhost","mangese","000000");
       echo "alert('$subquery');";
     //  $result= mysql_query(" $subquery "
     //  ." WHERE U_ID =$UID; ");
-     echo "alert('$subquery WHERE U_ID =$UID;');";
+      echo "alert('$subquery WHERE U_ID =$UID;');";
+    $result = mysql_query('$subquery WHERE U_ID =$UID;');
   }
 ?>
