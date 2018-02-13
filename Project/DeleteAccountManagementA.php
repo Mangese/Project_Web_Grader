b7b87afc115a83d7177d1f7ec782a216059cac37
theepakornkong <theepakornkong@gmail.com>
2018-02-13 22:27:18 +0700

    deleteAM

---
+++ Project/DeleteAccountManagementA.php
@@ -0,0 +1,11 @@
+<?php
+$conn = mysql_connect("localhost","mangese","000000");
+  if($conn != FALSE)
+  {
+    session_start();
+    mysql_query("use grader;");
+    mysql_query("set NAMES UTF8;");
+    $UID = $_REQUEST["uidDelete"];
+    echo "alert('in php');";
+  }
+?>
