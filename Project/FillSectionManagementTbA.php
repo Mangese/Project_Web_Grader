f002edb8c44ceb186a0cce04a0028b2693a20587
theepakornkong <theepakornkong@gmail.com>
2018-02-13 22:43:26 +0700

    Update FillSectionManagementTbA.php

--- Project/FillSectionManagementTbA.php
+++ Project/FillSectionManagementTbA.php
@@ -46,7 +46,7 @@
       echo "<button type='button' class='btn btn-warning btn-sm' data-toggle='modal' data-target='#editSection' onclick = 'editSectionManagementTb(this,$SIDT)';><i class='fa fa-edit' aria-hidden='true'></i></button>";
       echo "</td>";
       echo "<td style='width:10%'>";
-      echo "<button type='button' class='btn btn-danger btn-sm' onclick = 'deleteSectionManagement(this,$SIDT)'><i class='fa fa-trash' aria-hidden='true'></i></button>";      
+      echo "<button type='button' class='btn btn-danger btn-sm' data-toggle='modal' data-target='#modalChackDeleteSM' onclick = 'deleteSectionManagement(this,$SIDT)'><i class='fa fa-trash' aria-hidden='true'></i></button>";      
       echo "</td>";
       echo "</tr>";
     }
