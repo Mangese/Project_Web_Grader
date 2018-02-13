f3f54ad4357e5a57ea7f35d0f9c526bd0f155ad3
theepakornkong <theepakornkong@gmail.com>
2018-02-13 18:54:44 +0700

    Update EditAccountManagementA.php

--- Project/EditAccountManagementA.php
+++ Project/EditAccountManagementA.php
@@ -53,6 +53,6 @@ $conn = mysql_connect("localhost","mangese","000000");
      echo "alert($subquery);";
     //  $result= mysql_query(" $subquery "
     //  ." WHERE U_ID =$UID; ");
-    // mysql_query("insert into homework (P_ID,LANGUAGE,S_ID,AssignDate,AssignTime,DeadlineDate,DeadlineTime,FullMark) values ($PID,(select Language from problem where p_id = '1'),$SID,now(),now(),'$DATE','$TIME','$FULLMARK');");
+     mysql_query("$subquery WHERE U_ID =$UID;");
   }
 ?>
