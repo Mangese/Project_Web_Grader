5113f37b693be1b2d80778834affed2fd99b9611
theepakornkong <theepakornkong@gmail.com>
2018-02-13 22:39:03 +0700

    Update AdminPage.php

--- Project/AdminPage.php
+++ Project/AdminPage.php
@@ -393,15 +393,15 @@ if(!isset($_SESSION["user"]))
       var cidDelete = document.getElementById('cidmoc').value;
       alert(cidDelete);
 
-      // var xmlhttp = new XMLHttpRequest();
-      // xmlhttp.onreadystatechange = function () {
-      //   if (this.readyState == 4 && this.status == 200) {
-      //     alert("success");
-      //     eval(this.responseText);
-      //   }
-      // }
-      // xmlhttp.open("POST", "DeleteAccountManagementA.php?uidDelete=" + uidDelete, true);
-      // xmlhttp.send();
+      var xmlhttp = new XMLHttpRequest();
+      xmlhttp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+          alert("success");
+          eval(this.responseText);
+        }
+      }
+      xmlhttp.open("POST", "DeleteClassManagementA.php?cidDelete=" + cidDelete, true);
+      xmlhttp.send();
 
 
     }
