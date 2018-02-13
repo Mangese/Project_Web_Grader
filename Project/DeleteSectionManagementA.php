62677f4d1dcdbac5f90307ecbb301edfc7e37f7d
theepakornkong <theepakornkong@gmail.com>
2018-02-13 22:46:37 +0700

    deleteSM

---
+++ Project/DeleteSectionManagementA.php
@@ -0,0 +1,12 @@
+<?php
+$conn = mysql_connect("localhost","mangese","000000");
+  if($conn != FALSE)
+  {
+    session_start();
+    mysql_query("use grader;");
+    mysql_query("set NAMES UTF8;");
+    $SID = $_REQUEST["sidDelete"];
+    $SID1 = "'".$SID."'";
+    echo "alert('in DeleteSectionManagementA.php uid:' + $SID1);";
+  }
+?>
