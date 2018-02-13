59244cd976e79311e09b8bd226e2ac8b8a131cb0
theepakornkong <theepakornkong@gmail.com>
2018-02-13 07:33:02 +0700

    Update EditAccountManagementA.php

--- Project/EditAccountManagementA.php
+++ Project/EditAccountManagementA.php
@@ -34,11 +34,11 @@ $conn = mysql_connect("localhost","mangese","000000");
     // echo "alert('email '+ $EM);";
     // echo "alert('PW '+ $PW);";
     
-    
+    $tor="1111";
+    ."22222";
     //  mysql_query("use grader;");
      if ($UID!=''){
-      echo "alert('uid '+";
-      ." $UID);";
+      echo "alert($tor);";
      }
      
     // mysql_query("insert into homework (P_ID,LANGUAGE,S_ID,AssignDate,AssignTime,DeadlineDate,DeadlineTime,FullMark) values ($PID,(select Language from problem where p_id = '1'),$SID,now(),now(),'$DATE','$TIME','$FULLMARK');");
