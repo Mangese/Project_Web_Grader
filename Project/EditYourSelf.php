4bc617d86b120a47b8edc58f65b535b7a973bdf2
theepakornkong <theepakornkong@gmail.com>
2018-02-15 22:06:01 +0700

    Update EditYourSelf.php

--- Project/EditYourSelf.php
+++ Project/EditYourSelf.php
@@ -17,42 +17,42 @@
     if($UTS == 'T'){
       echo "alert('eieieieiei');";
       
-       $result = mysql_query("select u_id as uid,user_type as u_type,username as userName,(case when student_id is null then '' else student_id end) as Student_ID,firstname as firstName,lastname as lastName,department as department,email as Email from user where u_id like '$UIDS' ; ");
+      //  $result = mysql_query("select u_id as uid,user_type as u_type,username as userName,(case when student_id is null then '' else student_id end) as Student_ID,firstname as firstName,lastname as lastName,department as department,email as Email from user where u_id like '$UIDS' ; ");
     }else{
       echo "alert('5555555');";
       // $result = mysql_query("select u_id as uid,user_type as u_type,username as userName,(case when student_id is null then '' else student_id end) as Student_ID,firstname as firstName,lastname as lastName,department as department,email as Email from user where user_type like '%$TSR%' and student_id like '%$SIDSR%' and firstname like '%$NSR%' ; ");
     }
-    while($row = mysql_fetch_assoc($result))
-    {
+    // while($row = mysql_fetch_assoc($result))
+    // {
       
-      $UN = $row['userName'];
-      $SID = $row['Student_ID'];
-      $FN = $row['firstName'];
-      $LN = $row['lastName'];
-      $DM = $row['department'];
-      $EM = $row['Email'];
-      $UID = $row['uid'];
-      $UT = $row['u_type'];
+    //   $UN = $row['userName'];
+    //   $SID = $row['Student_ID'];
+    //   $FN = $row['firstName'];
+    //   $LN = $row['lastName'];
+    //   $DM = $row['department'];
+    //   $EM = $row['Email'];
+    //   $UID = $row['uid'];
+    //   $UT = $row['u_type'];
 
-      $UID1 = '"' .$UID. '"';
-      $SID1 = '"' .$SID. '"';
-      $UN1 = '"' .$UN. '"';
-      $FN1 = '"' .$FN. '"';
-      $LN1 = '"' .$LN. '"';
-      $DM1 = '"' .$DM. '"';
-      $EM1 = '"' .$EM. '"';
-      $UT1 = '"' .$UT. '"';
+    //   $UID1 = '"' .$UID. '"';
+    //   $SID1 = '"' .$SID. '"';
+    //   $UN1 = '"' .$UN. '"';
+    //   $FN1 = '"' .$FN. '"';
+    //   $LN1 = '"' .$LN. '"';
+    //   $DM1 = '"' .$DM. '"';
+    //   $EM1 = '"' .$EM. '"';
+    //   $UT1 = '"' .$UT. '"';
 
-      echo "alert('UID1:'+$UID1);";
-      echo "alert('SID1:'+$SID1);";
-      echo "alert('UN1:'+$UN1);";
-      echo "alert('FN1:'+$FN1);";
-      echo "alert('LN1:'+$LN1);";
-      echo "alert('DM1:'+$DM1);";
-      echo "alert('EM1:'+$EM1);";
-      echo "alert('UT1:'+$UT1);";
+    //   echo "alert('UID1:'+$UID1);";
+    //   echo "alert('SID1:'+$SID1);";
+    //   echo "alert('UN1:'+$UN1);";
+    //   echo "alert('FN1:'+$FN1);";
+    //   echo "alert('LN1:'+$LN1);";
+    //   echo "alert('DM1:'+$DM1);";
+    //   echo "alert('EM1:'+$EM1);";
+    //   echo "alert('UT1:'+$UT1);";
       
       
-    }
+    // }
      }
 ?>
