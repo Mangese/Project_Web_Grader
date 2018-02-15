0b89dfd27a37abf56278558c76176147332bbf94
theepakornkong <theepakornkong@gmail.com>
2018-02-15 18:48:06 +0700

    Add files via upload

---
+++ Project/EditYourSelf.php
@@ -0,0 +1,19 @@
+<?php
+// $conn = mysql_connect("localhost","mangese","000000");
+//   if($conn != FALSE)
+//   {
+    session_start();
+    
+    // $SID = $_REQUEST["sidreq"];
+    // $SN = $_REQUEST["sectionnameEdit"];
+    
+    
+    
+    echo "alert('inphp');";
+    // echo "alert('SN '+ $SN);";
+  
+    // mysql_query("use grader;");
+    // mysql_query("update section set name=$SN where s_id =$SID;");
+    
+    // }
+?>
