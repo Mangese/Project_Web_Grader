8ae049515fa15d8d8b6f990dd882bfd0b454bc6a
theepakornkong <theepakornkong@gmail.com>
2018-02-15 21:53:12 +0700

    Update EditYourSelf.php

--- Project/EditYourSelf.php
+++ Project/EditYourSelf.php
@@ -14,6 +14,34 @@
   
     // mysql_query("use grader;");
     // mysql_query("update section set name=$SN where s_id =$SID;");
-    
+    if($UT == 'T'){
+      echo "alert('eieieieiei');";
+      
+      // $result = mysql_query("select u_id as uid,user_type as u_type,username as userName,(case when student_id is null then '' else student_id end) as Student_ID,firstname as firstName,lastname as lastName,department as department,email as Email from user where user_type like '%$TSR%' and firstname like '%$NSR%' ; ");
+    }else{
+      echo "alert('5555555');";
+      // $result = mysql_query("select u_id as uid,user_type as u_type,username as userName,(case when student_id is null then '' else student_id end) as Student_ID,firstname as firstName,lastname as lastName,department as department,email as Email from user where user_type like '%$TSR%' and student_id like '%$SIDSR%' and firstname like '%$NSR%' ; ");
+    }
+    // while($row = mysql_fetch_assoc($result))
+    // {
+      
+    //   $UN = $row['userName'];
+    //   $SID = $row['Student_ID'];
+    //   $FN = $row['firstName'];
+    //   $LN = $row['lastName'];
+    //   $DM = $row['department'];
+    //   $EM = $row['Email'];
+    //   $UID = $row['uid'];
+    //   $UT = $row['u_type'];
+
+    //   $SID1 = '"' .$SID. '"';
+    //   $UN1 = '"' .$UN. '"';
+    //   $FN1 = '"' .$FN. '"';
+    //   $LN1 = '"' .$LN. '"';
+    //   $DM1 = '"' .$DM. '"';
+    //   $EM1 = '"' .$EM. '"';
+    //   $UT1 = '"' .$UT. '"';
+      
+    // }
      }
 ?>
