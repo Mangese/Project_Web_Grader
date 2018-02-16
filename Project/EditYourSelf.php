ca66d48c8e1244046f532559153eafa9a8bb17d8
theepakornkong <theepakornkong@gmail.com>
2018-02-16 19:25:17 +0700

    Update EditYourSelf.php

--- Project/EditYourSelf.php
+++ Project/EditYourSelf.php
@@ -27,32 +27,32 @@
     // {
       echo "alert('sucess');";
       
-    //   $UN = $row['userName'];
-    //   $SID = $row['Student_ID'];
-    //   $FN = $row['firstName'];
-    //   $LN = $row['lastName'];
-    //   $DM = $row['department'];
-    //   $EM = $row['Email'];
-    //   $UID = $row['uid'];
-    //   $UT = $row['u_type'];
+      $UN = $row['userName'];
+      $SID = $row['Student_ID'];
+      $FN = $row['firstName'];
+      $LN = $row['lastName'];
+      $DM = $row['department'];
+      $EM = $row['Email'];
+      $UID = $row['uid'];
+      $UT = $row['u_type'];
 
-    //   $UID1 = '"' .$UID. '"';
-    //   $SID1 = '"' .$SID. '"';
-    //   $UN1 = '"' .$UN. '"';
-    //   $FN1 = '"' .$FN. '"';
-    //   $LN1 = '"' .$LN. '"';
-    //   $DM1 = '"' .$DM. '"';
-    //   $EM1 = '"' .$EM. '"';
-    //   $UT1 = '"' .$UT. '"';
+      $UID1 = '"' .$UID. '"';
+      $SID1 = '"' .$SID. '"';
+      $UN1 = '"' .$UN. '"';
+      $FN1 = '"' .$FN. '"';
+      $LN1 = '"' .$LN. '"';
+      $DM1 = '"' .$DM. '"';
+      $EM1 = '"' .$EM. '"';
+      $UT1 = '"' .$UT. '"';
 
-    //   echo "alert('UID1:'+$UID1);";
-    //   echo "alert('SID1:'+$SID1);";
-    //   echo "alert('UN1:'+$UN1);";
-    //   echo "alert('FN1:'+$FN1);";
-    //   echo "alert('LN1:'+$LN1);";
-    //   echo "alert('DM1:'+$DM1);";
-    //   echo "alert('EM1:'+$EM1);";
-    //   echo "alert('UT1:'+$UT1);";
+      echo "alert('UID1:'+$UID1);";
+      echo "alert('SID1:'+$SID1);";
+      echo "alert('UN1:'+$UN1);";
+      echo "alert('FN1:'+$FN1);";
+      echo "alert('LN1:'+$LN1);";
+      echo "alert('DM1:'+$DM1);";
+      echo "alert('EM1:'+$EM1);";
+      echo "alert('UT1:'+$UT1);";
       
       
      }
