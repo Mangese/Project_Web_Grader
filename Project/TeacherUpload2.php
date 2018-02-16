709516f35582fb32cd881653a9354a8cd8845079
theepakornkong <theepakornkong@gmail.com>
2018-02-16 20:37:49 +0700

    Update TeacherUpload2.php

--- Project/TeacherUpload2.php
+++ Project/TeacherUpload2.php
@@ -393,16 +393,16 @@
 
 
 
-                      // var xmlhttp = new XMLHttpRequest();
-                      // xmlhttp.onreadystatechange = function () {
-                      //   if (this.readyState == 4 && this.status == 200) {
-                      //     // alert("success");
-                      //     eval(this.responseText);
-                      //   }
-                      // }
-                      // xmlhttp.open("POST", "EditAccountManagementA.php?uidreq=" + uidreq + fnamesend + lnamesend + unamesend + sidsend + departsend + emailsend + passSend + "&utypereq=" + utypereq, true);
-                      // xmlhttp.send();
-                      // location.reload();
+                      var xmlhttp = new XMLHttpRequest();
+                      xmlhttp.onreadystatechange = function () {
+                        if (this.readyState == 4 && this.status == 200) {
+                          // alert("success");
+                          eval(this.responseText);
+                        }
+                      }
+                      xmlhttp.open("POST", "EditAccountManagementA.php?uidreq=" + uidreq + fnamesend + lnamesend + unamesend + sidsend + departsend + emailsend + passSend + "&utypereq=" + utypereq, true);
+                      xmlhttp.send();
+                      location.reload();
                     }
                     else {
                       document.getElementById("uidmoc").value = "";
