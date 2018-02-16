4de962a141f9caf1a68e177eddb6aa2fc42d8d2f
theepakornkong <theepakornkong@gmail.com>
2018-02-16 19:27:38 +0700

    Update EditYourSelf.php

--- Project/EditYourSelf.php
+++ Project/EditYourSelf.php
@@ -23,8 +23,8 @@
     }
      if($result)
      {
-    // while($row = mysql_fetch_assoc($result))
-    // {
+     while($row = mysql_fetch_assoc($result))
+     {
       echo "alert('sucess');";
       
       $UN = $row['userName'];
@@ -45,14 +45,14 @@
       $EM1 = '"' .$EM. '"';
       $UT1 = '"' .$UT. '"';
 
-      echo "alert('UID1:'+$UID);";
-      echo "alert('SID1:'+$SID);";
-      echo "alert('UN1:'+$UN);";
-      echo "alert('FN1:'+$FN);";
-      echo "alert('LN1:'+$LN);";
-      echo "alert('DM1:'+$DM);";
-      echo "alert('EM1:'+$EM);";
-      echo "alert('UT1:'+$UT);";
+      echo "alert('UID1:'+$UID1);";
+      echo "alert('SID1:'+$SID1);";
+      echo "alert('UN1:'+$UN1);";
+      echo "alert('FN1:'+$FN1);";
+      echo "alert('LN1:'+$LN1);";
+      echo "alert('DM1:'+$DM1);";
+      echo "alert('EM1:'+$EM1);";
+      echo "alert('UT1:'+$UT1);";
       
       
      }
