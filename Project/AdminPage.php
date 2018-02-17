eaf14ba9cc5ce546418520d711f7cac765a2a155
Mangese <chainarhong2@gmail.com>
2018-02-17 20:58:36 +0700

    Update AdminPage.php

--- Project/AdminPage.php
+++ Project/AdminPage.php
@@ -1129,7 +1129,7 @@ if(!isset($_SESSION["user"]))
               //alert("end date is: " + edate);
               //alert("type file is: " + tFile);
               //alert(tFilesearch);
-
+		$('#FileManagementTb tbody tr').remove();
               var xmlhttp = new XMLHttpRequest();
               xmlhttp.onreadystatechange = function () {
                 if (this.readyState == 4 && this.status == 200) {
