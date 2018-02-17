0d39a883c5aa57f56034ca06b2b3f4733f7a2274
Mangese <chainarhong2@gmail.com>
2018-02-17 21:49:13 +0700

    Update DeleteFile.php

--- Project/DeleteFile.php
+++ Project/DeleteFile.php
@@ -3,7 +3,8 @@
   $conn = mysql_connect("localhost","mangese","000000");
 if($conn != FALSE)
 {
-  mysql_query("delete from submit where source_file = '$F';";
-  exec("rm File/$F ");
+  echo "alert('$F');";
+ // mysql_query("delete from submit where source_file = '$F';";
+ // exec("rm File/$F ");
 }
 ?>
