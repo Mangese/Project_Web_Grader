c74bab415dda69cea9b65ddbc161579caebaa3e9
Mangese <chainarhong2@gmail.com>
2018-02-17 22:14:07 +0700

    Update DeleteFile.php

--- Project/DeleteFile.php
+++ Project/DeleteFile.php
@@ -6,10 +6,8 @@ if($conn != FALSE)
   mysql_query("use grader;");
 
   echo "alert('$F');";
-  mysql_query("delete from submit where source_file = '$F';",$ERR);
+  mysql_query("delete from submit where source_file = '$F';");
   exec("rm File/$F ",$out,$err);
   echo "alert('$err');";
-  $EM = mysql_error($ERR);
-  echo "alert('$ERR');";
 }
 ?>
