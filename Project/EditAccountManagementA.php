d941f9943fe45ad6d59c9a8917541ddd0ade390d
theepakornkong <theepakornkong@gmail.com>
2018-02-17 22:14:33 +0700

    Update EditAccountManagementA.php

--- Project/EditAccountManagementA.php
+++ Project/EditAccountManagementA.php
@@ -10,11 +10,11 @@ $conn = mysql_connect("localhost","mangese","000000");
     $FNE = $_REQUEST["fnameEdit"];
     $LNE = $_REQUEST["lnameEdit"];
 
-    // $FNE1 = "'".$FNE."'";
-    // $LNE1 = "'".$LNE."'";
+    $FNE1 = "'".$FNE."'";
+    $LNE1 = "'".$LNE."'";
     echo "alert('in php');";
-      echo "alert('$FNE')";
-      echo "alert('$LNE')";
+      echo "alert('fne : ' +$FNE1)";
+      echo "alert('lne : ' +$LNE1)";
     
     $UID = $_REQUEST["uidreq"];
     $UT = $_REQUEST["utypereq"];
