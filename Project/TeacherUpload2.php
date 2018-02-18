bec419a59899c6545bf220f31439f52b8708c62f
tammopinkid <tammo.pinkid@gmail.com>
2018-02-18 23:01:33 +0700

    edit datepicker

--- Project/TeacherUpload2.php
+++ Project/TeacherUpload2.php
@@ -28,7 +28,8 @@
 
   <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.18.1/moment.js"></script>
 
-  <!-- <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datetimepicker/4.17.47/js/bootstrap-datetimepicker.min.js"></script> -->
+  <!-- Date Picker -->
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datetimepicker/4.17.47/js/bootstrap-datetimepicker.min.js"></script>
 
   <!-- Date Picker -->
   <script src="https://cdn.jsdelivr.net/gh/atatanasov/gijgo@1.8.0/dist/combined/js/gijgo.min.js" type="text/javascript"></script>
