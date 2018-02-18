49f1fb312881b92cad2cb3190927ebbf9b7889cc
Mangese <chainarhong2@gmail.com>
2018-02-18 21:03:18 +0700

    Update TeacherUpload2.php

--- Project/TeacherUpload2.php
+++ Project/TeacherUpload2.php
@@ -742,7 +742,7 @@
 
     <script>
       $(document).ready(function () {
-       // $('[data-toggle="tooltip"]').tooltip();
+        $('[data-toggle="tooltip"]').tooltip();
       });
     </script>
     <!--End script-->
