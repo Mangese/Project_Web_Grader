2eb8cf727dccc343ab64fe083e96d1f4e65137de
Mangese <chainarhong2@gmail.com>
2018-02-18 20:39:58 +0700

    Update TeacherUpload2.php

--- Project/TeacherUpload2.php
+++ Project/TeacherUpload2.php
@@ -510,15 +510,9 @@
 		    }
 		    else
 		    {
-		   var xmlhttp = new XMLHttpRequest();
-                    xmlhttp.onreadystatechange = function () {
-                      if (this.readyState == 4 && this.status == 200) {
-                        eval(this.responseText);
-			      fillTableResult();
-                      }
-                    }
-                    xmlhttp.open("POST", "FillSetMark.php?uidreq=" + uidreq + "&pidreq=" + pidreq + "&setMark=" + setMark, true);
-                    xmlhttp.send();
+			   alert("Success");
+			    alert(setMark);
+			    alert(fullmark);
 			    
 		    }
                     
