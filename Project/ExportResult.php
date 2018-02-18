9419b7c42fdd8cd66bd2fc8fb29096a0d74f7f69
Mangese <chainarhong2@gmail.com>
2018-02-18 16:18:18 +0700

    Create ExportResult.php

---
+++ Project/ExportResult.php
@@ -0,0 +1,15 @@
+<?php
+header('Content-Disposition: attachment; filename="default-filename.csv"');
+  $conn = mysql_connect("localhost","mangese","000000");
+	mysql_query("use grader;");
+  $SID = $_REQUEST['sid'];
+  echo "ID,Name";
+  $NumberOfFile = mysql_query("select count(*) as num from homework where s_id = '$SID';");
+  while($row = mysql_fetch_assoc($NumberOfFile))
+  {
+  $NumFile = $row['num'];
+  echo "Ex".$NumFile.",";
+  echo "Full Mark For Ex".$NumFile.",";
+  }
+  echo "TotalPass,SumPoints";
+?>
