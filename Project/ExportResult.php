c03fa966acfd62e14d57a73585bf94e2f6938031
Mangese <chainarhong2@gmail.com>
2018-02-18 19:26:59 +0700

    Update ExportResult.php

--- Project/ExportResult.php
+++ Project/ExportResult.php
@@ -60,15 +60,17 @@ header('Content-Disposition: attachment; filename="default-filename.csv"');
             if (!strcmp($STATUS,"P")){
               $sumPass = $sumPass+1;
 //               echo "<i class='fa fa-check' aria-hidden='true' style='color:#2ECC71'></i>";
-              echo "Pass,";
+              echo "Pass";
               if($teacherMark != ''){
-                echo "$teacherMark pt.,";
+                echo "$teacherMark pt.";
               }
+		    echo ",";
             }
             else {
-              echo "Not Pass,";
+              echo "Not Pass";
               if($teacherMark != ''){
-                 echo "$teacherMark pt.,";
+                 echo "$teacherMark pt.";
+		      echo ",";
               }
 //               echo '<i class="fa fa-times" aria-hidden="true" style="color:#E74C3C" onclick = ResultModalHeader($ID,$HidModal,$CountRowForModal,$numprob,"' . $NAME. '"); data-toggle="modal" data-target="#modalSourceFileSend">''</i>';
               
