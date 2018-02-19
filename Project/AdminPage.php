276fecab53596b780f1fd1178ee67edb32508953
theepakornkong <theepakornkong@gmail.com>
2018-02-19 12:05:44 +0700

    Update AdminPage.php

--- Project/AdminPage.php
+++ Project/AdminPage.php
@@ -580,6 +580,7 @@ if(!isset($_SESSION["user"]))
             <label>Select type</label>
             <select class="form-control ml-3" id="selectType" onchange="selectTypeOnChange()">
                 <option value="">All type</option>
+                <option value="A">Admin</option>
                 <option value="T">Lecturer</option>
                 <option value="S">Student</option>
                 
@@ -1255,8 +1256,8 @@ if(!isset($_SESSION["user"]))
 
         <form class="form-inline mx-2 mb-3" style="margin-top:20px">
           <div class="form-inline">
-            <input type="radio" style="display:none;" name="typeFile" value="flag" checked> Flag &emsp;
-            <input type="radio" style="display:none;" name="typeFile" value="fileSubmit"> File Submit &emsp;
+            <input type="radio" style="display:none;" name="typeFile" value="flag" checked>
+            <input type="radio" style="display:none;" name="typeFile" value="fileSubmit">
             <label class="mr-3">Start Date :</label>
             <input id="startDate" width="200" />
             <label class="ml-3 mr-3">End Date :</label>
