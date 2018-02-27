b50a3b769a2e60a7aaaab1b5f1a52f38960d4da8
tammopinkid <benjarat.saeang@gmail.com>
2018-02-28 01:29:28 +0700

    edit tooltip

--- Project/TeacherUpload2.php
+++ Project/TeacherUpload2.php
@@ -1670,13 +1670,13 @@
                       </ol>
                       <div class="carousel-inner" role="listbox">
                         <div class="carousel-item active" >
-                          <img class="d-block img-fluid" src="./images/results.jpg" alt="First slide">
+                          <img class="d-block img-fluid" src="./images/tresult.jpg" alt="First slide">
                         </div>
                         <div class="carousel-item">
                           <img class="d-block img-fluid" src="./images/lastFileSent.jpg" alt="Second slide">
                         </div>
                         <div class="carousel-item">
-                          <img class="d-block img-fluid" src="./images/csv.jpg" alt="Third slide">
+                          <img class="d-block img-fluid" src="./images/copyDetection.jpg" alt="Third slide">
                         </div>
                       </div>
                       <a class="carousel-control-prev" href="#carouselExampleIndicators3" role="button" data-slide="prev">
