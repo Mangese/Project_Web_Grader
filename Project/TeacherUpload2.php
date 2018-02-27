53f0ecbcd3afa2902137a582c367d2598194a368
Mangese <chainarhong2@gmail.com>
2018-02-27 17:55:51 +0700

    Update TeacherUpload2.php

--- Project/TeacherUpload2.php
+++ Project/TeacherUpload2.php
@@ -87,7 +87,7 @@
     }
 .bootstrap-datetimepicker-widget.dropdown-menu.usetwentyfour.bottom{
       box-sizing: initial;
-      width: 106.5%;
+      width: 200%;
     }
 
     .bootstrap-datetimepicker-widget table {
