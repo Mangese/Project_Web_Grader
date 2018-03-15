7f618e4b539f629eb7686ba903f8caf9488bbd8d
Mangese <chainarhong2@gmail.com>
2018-03-16 00:16:46 +0700

    Update DeleteAccountManagementA.php

--- Project/DeleteAccountManagementA.php
+++ Project/DeleteAccountManagementA.php
@@ -9,12 +9,28 @@ $conn = mysql_connect("localhost","mangese","000000");
     while($row = mysql_fetch_assoc($result))
     {
     $F = $row["filename"];
-      exec("rm File/$F1 ");
+      exec("rm File/$F ");
 
     }
+    $result = mysql_query("select s_id as sid from section where u_id = $UID;");
+    while($row = mysql_fetch_assoc($result))
+    {
+    $SID = $row["sid"];
+    $result1 = mysql_query("select source_file as filename from submit where s_id = $SID;");
+    while($row1 = mysql_fetch_assoc($result))
+    {
+      $F = $row1["filename"];
+      exec("rm File/$F ");
+    }
+    mysql_query("delete from homework where s_id = '$SID';");
+    mysql_query("delete from register where s_id = '$SID';");
+    mysql_query("delete from submit where s_id = '$SID';");
+    }
+    
         mysql_query("delete from submit where u_id = '$UID';");
         mysql_query("delete from register where u_id = '$UID';");
         mysql_query("delete from user where u_id = '$UID';");
+        mysql_query("delete from section where u_id = '$UID';");
         echo "fillaccountManagementTb();";
 
   }
