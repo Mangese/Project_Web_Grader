6b35a1bea0cead30175f0b277ee810ce29b2d2a3
Mangese <chainarhong2@gmail.com>
2018-04-03 15:24:30 +0700

    Update Core.php

--- Project/Core.php
+++ Project/Core.php
@@ -1,4 +1,13 @@
 <?php
+$target_dir = "File/";
+$target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
+$uploadOk = 1;
+$imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+// Check if image file is a actual image or fake image
+if(isset($_POST["submit"])) {
+
+    echo "upload ok";
+}
 	echo "<script> alert('Success'); </script>";
 /*
 session_start();
